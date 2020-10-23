afdf4fea70732f1a51690a6bc4a6dcc4ae12cfe4
Gaël Bonithon <trash.paradise@protonmail.com>
2020-10-23 21:22:43 +0200

    Switch to schema-id format for the setting names

--- mousepad/mousepad-private.h
+++ mousepad/mousepad-private.h
@@ -58,8 +58,12 @@ enum
   ERROR_FILE_STATUS_FAILED = -4
 };
 
+/* Mousepad id, common to application, icon, settings, etc. */
+#define MOUSEPAD_ID     "org.xfce.mousepad"
+#define MOUSEPAD_ID_LEN G_N_ELEMENTS (MOUSEPAD_ID) - 1
+
 /* config file locations */
-#define MOUSEPAD_RC_RELPATH     ("Mousepad" G_DIR_SEPARATOR_S "mousepadrc")
+#define MOUSEPAD_RC_RELPATH ("Mousepad" G_DIR_SEPARATOR_S "mousepadrc")
 
 /* handling flags */
 #define MOUSEPAD_SET_FLAG(flags,flag)   G_STMT_START{ ((flags) |= (flag)); }G_STMT_END
