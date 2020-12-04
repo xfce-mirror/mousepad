dd81bf02243ab292774b3eba661949c5e88beac9
Gaël Bonithon <gael@xfce.org>
2020-12-04 18:01:45 +0100

    Revert renaming of "font-name" setting
    To prioritize backward compatibility over naming consistency, and since
    it is possible to circumscribe this inconsistency quite closely.
    
    See https://gitlab.xfce.org/apps/mousepad/-/merge_requests/60#note_19670

--- mousepad/mousepad-settings.h
+++ mousepad/mousepad-settings.h
@@ -23,7 +23,7 @@ G_BEGIN_DECLS
 
 /* Setting names */
 #define MOUSEPAD_SETTING_AUTO_INDENT                  "preferences.view.auto-indent"
-#define MOUSEPAD_SETTING_FONT                         "preferences.view.font"
+#define MOUSEPAD_SETTING_FONT                         "preferences.view.font-name"
 #define MOUSEPAD_SETTING_USE_DEFAULT_FONT             "preferences.view.use-default-monospace-font"
 #define MOUSEPAD_SETTING_SHOW_WHITESPACE              "preferences.view.show-whitespace"
 #define MOUSEPAD_SETTING_SHOW_WHITESPACE_LEADING      "preferences.view.show-whitespace.leading"
