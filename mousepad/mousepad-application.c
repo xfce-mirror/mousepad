668b932a917268230a9db2a03bcf38c6556ffc50
Gaël Bonithon <gael@xfce.org>
2020-12-12 13:41:22 +0100

    Let GFile APIs check for file exitence

--- mousepad/mousepad-application.c
+++ mousepad/mousepad-application.c
@@ -796,7 +796,7 @@ mousepad_application_open (GApplication  *gapplication,
           window = mousepad_application_get_window_for_files (application);
 
           /* open the files */
-          opened = mousepad_window_open_files (MOUSEPAD_WINDOW (window), valid_files, valid);
+          opened = mousepad_window_open_files (MOUSEPAD_WINDOW (window), valid_files, valid, FALSE);
 
           /* if at least one file was finally opened, show the window */
           if (opened > 0)
@@ -813,7 +813,7 @@ mousepad_application_open (GApplication  *gapplication,
               window = mousepad_application_create_window (application);
 
               /* open the file */
-              opened = mousepad_window_open_files (MOUSEPAD_WINDOW (window), valid_files + n, 1);
+              opened = mousepad_window_open_files (MOUSEPAD_WINDOW (window), valid_files + n, 1, FALSE);
 
               /* if the file was finally opened, show the window */
               if (opened > 0)
