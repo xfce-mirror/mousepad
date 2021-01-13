8bad334f40589ec8b5ce3dc8befeece875edc98a
Gaël Bonithon <gael@xfce.org>
2021-01-13 20:04:13 +0100

    Update copyright year

--- mousepad/mousepad-application.c
+++ mousepad/mousepad-application.c
@@ -384,8 +384,8 @@ mousepad_application_handle_local_options (GApplication *gapplication,
   if (g_variant_dict_contains (options, "version"))
     {
       g_print ("%s %s\n\n", PACKAGE_NAME, PACKAGE_VERSION);
-      g_print ("%s\n", "Copyright (c) 2007");
-      g_print ("\t%s\n\n", _("The Xfce development team. All rights reserved."));
+      g_print ("%s\n", "Copyright (C) 2005-2021");
+      g_print ("\t%s\n\n", _("The Mousepad developers. All rights reserved."));
       g_print (_("Please report bugs to <%s>."), PACKAGE_BUGREPORT);
       g_print ("\n");
 
