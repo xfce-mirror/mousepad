7fd11a7b836c8e85ea2f22461ea5cc0148639bb5
Gaël Bonithon <gael@xfce.org>
2021-02-17 12:59:24 +0100

    Pre-GTK 4: Remove obsolete code

--- mousepad/mousepad-file.c
+++ mousepad/mousepad-file.c
@@ -318,7 +318,7 @@ mousepad_file_set_monitor (gpointer data)
       if ((file->symlink = mousepad_util_is_symlink (file->location)))
         {
           /* try to get the final target */
-          path = realpath (mousepad_util_get_path (file->location), NULL);
+          path = realpath (g_file_peek_path (file->location), NULL);
 
           /* this is a broken link: we have to use readlink() to get the final target */
           if (path == NULL && g_file_error_from_errno (errno) == G_FILE_ERROR_NOENT)
@@ -434,7 +434,7 @@ mousepad_file_set_location (MousepadFile *file,
             ! g_file_info_get_attribute_boolean (fileinfo, G_FILE_ATTRIBUTE_ACCESS_CAN_WRITE));
           g_object_unref (fileinfo);
         }
-      else if (mousepad_util_get_path (location) == NULL)
+      else if (g_file_peek_path (location) == NULL)
         mousepad_file_set_read_only (file, TRUE);
       else
         mousepad_file_set_read_only (file, FALSE);
@@ -476,7 +476,7 @@ mousepad_file_get_path (MousepadFile *file)
 {
   g_return_val_if_fail (MOUSEPAD_IS_FILE (file), NULL);
 
-  return mousepad_util_get_path (file->location);
+  return g_file_peek_path (file->location);
 }
 
 
