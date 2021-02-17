6ff827f4a28286e3440425222bdb1c2866b8473a
Gaël Bonithon <gael@xfce.org>
2021-02-17 12:59:24 +0100

    Pre-GTK 4: Remove obsolete code

--- mousepad/mousepad-util.h
+++ mousepad/mousepad-util.h
@@ -94,8 +94,6 @@ GSList      *mousepad_util_get_sorted_languages_for_section (const gchar        
  */
 gchar       *mousepad_util_pango_font_description_to_css    (const PangoFontDescription *font_desc);
 
-const gchar *mousepad_util_get_path                         (GFile                      *file);
-
 gpointer     mousepad_util_source_autoremove                (gpointer                    object);
 
 void         mousepad_util_recent_init                      (void);
