323b7a79a13ce88b0e6fff39e09c25ad341bddea
Gaël Bonithon <gael@xfce.org>
2021-02-18 11:59:08 +0100

    GTK 4: Adapt to GtkStyleContext/GtkCssProvider API changes

--- mousepad/mousepad-view.c
+++ mousepad/mousepad-view.c
@@ -1372,7 +1372,7 @@ mousepad_view_set_font (MousepadView *view,
 
   /* set font */
   provider = gtk_css_provider_new ();
-  gtk_css_provider_load_from_data (provider, css_string, -1, NULL);
+  gtk_css_provider_load_from_data (provider, css_string, -1);
   gtk_style_context_add_provider (gtk_widget_get_style_context (GTK_WIDGET (view)),
                                   GTK_STYLE_PROVIDER (provider),
                                   GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
