9c9865846abf100516d96a7516c5536fb519600f
Gaël Bonithon <gael@xfce.org>
2021-02-21 20:33:17 +0100

    GTK 4: TODO (likely upstream bugs)

--- mousepad/mousepad-window.c
+++ mousepad/mousepad-window.c
@@ -3083,6 +3083,8 @@ mousepad_window_can_redo (GtkTextBuffer  *buffer,
 /**
  * Menu Functions
  **/
+/* TODO: same bug as for mousepad_application_create_languages_menu() */
+#if 0
 static void
 mousepad_window_menu_templates_fill (MousepadWindow *window,
                                      GMenu          *menu,
@@ -3215,7 +3217,7 @@ mousepad_window_menu_templates_fill (MousepadWindow *window,
       g_object_unref (item);
     }
 }
-
+#endif
 
 
 static void
@@ -3223,6 +3225,8 @@ mousepad_window_menu_templates (GSimpleAction *action,
                                 GVariant      *state,
                                 gpointer       data)
 {
+/* TODO: same bug as for mousepad_application_create_languages_menu() */
+#if 0
   GtkApplication *application;
   GMenu          *menu;
   GMenuItem      *item;
@@ -3286,6 +3290,7 @@ mousepad_window_menu_templates (GSimpleAction *action,
       /* unlock */
       lock_menu_updates--;
     }
+#endif
 }
 
 
@@ -3528,6 +3533,8 @@ mousepad_window_update_gomenu (GSimpleAction *action,
 
 
 
+/* TODO: same bug as for mousepad_application_create_languages_menu() */
+#if 0
 /* sort list in descending order */
 static gint
 mousepad_window_recent_sort (gconstpointer ga,
@@ -3536,7 +3543,7 @@ mousepad_window_recent_sort (gconstpointer ga,
   return g_date_time_compare (gtk_recent_info_get_modified ((GtkRecentInfo *) ga),
                               gtk_recent_info_get_modified ((GtkRecentInfo *) gb));
 }
-
+#endif
 
 
 static void
@@ -3544,6 +3551,8 @@ mousepad_window_recent_menu (GSimpleAction *action,
                              GVariant      *state,
                              gpointer       data)
 {
+/* TODO: same bug as for mousepad_application_create_languages_menu() */
+#if 0
   GtkApplication   *application;
   GtkRecentManager *manager;
   GtkRecentInfo    *info;
@@ -3673,6 +3682,7 @@ mousepad_window_recent_menu (GSimpleAction *action,
       mousepad_window_menu_update_tooltips (G_MENU_MODEL (menu), 0, 0, 0, data);
       lock_menu_updates--;
     }
+#endif
 }
 
 
