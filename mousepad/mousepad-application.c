7df374809acd4d312295699241d097f08a1b33a5
Gaël Bonithon <gael@xfce.org>
2021-02-21 20:33:17 +0100

    GTK 4: TODO (likely upstream bugs)

--- mousepad/mousepad-application.c
+++ mousepad/mousepad-application.c
@@ -1638,6 +1638,9 @@ mousepad_application_set_shared_menu_parts (MousepadApplication *application,
 static void
 mousepad_application_create_languages_menu (MousepadApplication *application)
 {
+/* TODO: prevents the loading of the menubar (neverending) and also affects that of
+ * the status bar languages menu -> GTK bug https://gitlab.gnome.org/GNOME/gtk/-/issues/3718 */
+#if 0
   GMenu       *menu, *submenu;
   GMenuItem   *item;
   GSList      *sections, *languages, *iter_sect, *iter_lang;
@@ -1689,6 +1692,7 @@ mousepad_application_create_languages_menu (MousepadApplication *application)
     }
 
   g_slist_free (sections);
+#endif
 }
 
 
