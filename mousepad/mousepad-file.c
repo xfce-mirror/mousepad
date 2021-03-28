5bfc12bb9130cd0b4b4759000f71323d767d6506
Avinash Sonawane <rootkea@gmail.com>
2021-03-29 01:11:22 +0530

    Support negative --line and --column values
    1. For negative line number count from bottom towards top.
    If LINE is negative and its absolute value is more than or
    equal to total number of lines in buffer then LINE gets set to 1.
    
    2. For negative column number count from line end to line start.
    If COLUMN is negative and its absolute value is more than the
    total number of columns in line then COLUMN gets set to 0.

--- mousepad/mousepad-file.c
+++ mousepad/mousepad-file.c
@@ -517,7 +517,8 @@ mousepad_file_open (MousepadFile  *file,
   const gchar      *charset, *bom_charset, *endc, *n, *m;
   gchar            *contents = NULL, *etag, *temp;
   gsize             file_size, written, bom_length;
-  gint              retval = ERROR_READING_FAILED;
+  gint              tab_width, retval = ERROR_READING_FAILED;
+  gboolean          from_end = FALSE;
 
   g_return_val_if_fail (MOUSEPAD_IS_FILE (file), FALSE);
   g_return_val_if_fail (GTK_IS_TEXT_BUFFER (file->buffer), FALSE);
@@ -667,8 +668,28 @@ mousepad_file_open (MousepadFile  *file,
           if (G_LIKELY (n - m > 0))
             gtk_text_buffer_insert (file->buffer, &start, m, n - m);
 
-          /* get the position iter */
-          gtk_text_buffer_get_iter_at_line_offset (file->buffer, &pos, line, column);
+          /* For negative line number count from bottom */
+          if (line < 0)
+            line = MAX (gtk_text_buffer_get_line_count (file->buffer) + line, 0);
+
+          /* set the position iter line */
+          gtk_text_buffer_get_iter_at_line (file->buffer, &pos, line);
+
+          /* get the tab width */
+          tab_width = MOUSEPAD_SETTING_GET_INT (TAB_WIDTH);
+
+          /* For negative column number count from end of the line */
+          if (column < 0)
+            {
+              if (!gtk_text_iter_ends_line (&pos))
+                gtk_text_iter_forward_to_line_end (&pos);
+
+              column = MAX (mousepad_util_get_real_line_offset (&pos, tab_width) + column + 1, 0);
+              from_end = TRUE;
+            }
+
+          /* set the position iter offset */
+          mousepad_util_set_real_line_offset (&pos, tab_width, column, from_end);
 
           /* set the cursor at the position iter */
           gtk_text_buffer_place_cursor (file->buffer, &pos);
