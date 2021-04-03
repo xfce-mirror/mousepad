50b8937c94eeb893c58889ebad98396dc42964ff
Avinash Sonawane <rootkea@gmail.com>
2021-04-03 19:53:08 +0530

    Support -ve line and column values for "Go to" location

--- mousepad/mousepad-util.c
+++ mousepad/mousepad-util.c
@@ -16,6 +16,7 @@
 
 #include <mousepad/mousepad-private.h>
 #include <mousepad/mousepad-util.h>
+#include <mousepad/mousepad-settings.h>
 
 #include <xfconf/xfconf.h>
 
@@ -531,14 +532,15 @@ mousepad_util_dialog_update_header (GtkDialog   *dialog,
 
 
 gint
-mousepad_util_get_real_line_offset (const GtkTextIter *iter,
-                                    gint               tab_size)
+mousepad_util_get_real_line_offset (const GtkTextIter *iter)
 {
-  gint        offset = 0;
+  gint        tab_size, offset = 0;
   GtkTextIter needle = *iter;
 
   /* move the needle to the start of the line */
   gtk_text_iter_set_line_offset (&needle, 0);
+
+  tab_size = MOUSEPAD_SETTING_GET_INT (TAB_WIDTH);
 
   /* forward the needle until we hit the iter */
   while (!gtk_text_iter_equal (&needle, iter))
@@ -558,17 +560,18 @@ mousepad_util_get_real_line_offset (const GtkTextIter *iter,
 
 
 
-void
+static void
 mousepad_util_set_real_line_offset (GtkTextIter *iter,
-                                    gint         tab_size,
                                     gint         column,
                                     gboolean     from_end)
 {
-  gint        char_offset = 0, column_offset = 0;
+  gint        tab_size, char_offset = 0, column_offset = 0;
   GtkTextIter needle = *iter;
 
   /* move the needle to the start of the line */
   gtk_text_iter_set_line_offset (&needle, 0);
+
+  tab_size = MOUSEPAD_SETTING_GET_INT (TAB_WIDTH);
 
   /* forward the needle until we reach column or the end of the line */
   while (!gtk_text_iter_ends_line (&needle) && column_offset < column)
@@ -599,6 +602,40 @@ mousepad_util_set_real_line_offset (GtkTextIter *iter,
 
   /* set iter at char_offset */
   gtk_text_iter_set_line_offset (iter, char_offset);
+}
+
+
+
+void
+mousepad_util_place_cursor (GtkTextBuffer *buffer,
+                            gint           line,
+                            gint           column)
+{
+  GtkTextIter pos;
+  gboolean    from_end = FALSE;
+
+  /* for negative line number count from bottom */
+  if (line < 0)
+    line = MAX (gtk_text_buffer_get_line_count (buffer) + line, 0);
+
+  /* set the position iter line */
+  gtk_text_buffer_get_iter_at_line (buffer, &pos, line);
+
+  /* for negative column number count from end of the line */
+  if (column < 0)
+    {
+      if (!gtk_text_iter_ends_line (&pos))
+        gtk_text_iter_forward_to_line_end (&pos);
+
+      column = MAX (mousepad_util_get_real_line_offset (&pos) + column + 1, 0);
+      from_end = TRUE;
+    }
+
+  /* set the position iter offset */
+  mousepad_util_set_real_line_offset (&pos, column, from_end);
+
+  /* set the cursor at the position iter */
+  gtk_text_buffer_place_cursor (buffer, &pos);
 }
 
 
