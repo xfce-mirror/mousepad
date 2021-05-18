xfce-mirror/mousepad

commit c5200610bad2a34f940e1fdbb1f0d70c104e0537

Automate and sanitize memory management of sources

mousepad/mousepad-window.c
@@ -1990,7 +1990,8 @@ mousepad_window_open_file (MousepadWindow   *window,
 
             /* scroll to cursor if -l or -c is used */
             if (line != 0 || column != 0)
-              g_idle_add (mousepad_view_scroll_to_cursor, window->active->textview);
+              g_idle_add (mousepad_view_scroll_to_cursor,
+                          mousepad_util_source_autoremove (window->active->textview));
 
             /* insert in the recent history */
             mousepad_window_recent_add (window, document->file);
@@ -2857,17 +2858,11 @@ mousepad_window_notebook_create_window (GtkNotebook    *notebook,
 static gboolean
 mousepad_window_pending_widget_idle (gpointer data)
 {
-  MousepadDocument *document;
+  MousepadDocument *document = data;
   MousepadWindow   *window;
 
-  /* the window or its attributes may no longer exist when we get here */
-  if (MOUSEPAD_IS_DOCUMENT (data))
-    {
-      document = MOUSEPAD_DOCUMENT (data);
-      window = MOUSEPAD_WINDOW (gtk_widget_get_ancestor (GTK_WIDGET (data), MOUSEPAD_TYPE_WINDOW));
-      if (MOUSEPAD_IS_WINDOW (window) && MOUSEPAD_IS_FILE (document->file))
-        mousepad_window_externally_modified (document->file, window);
-    }
+  window = MOUSEPAD_WINDOW (gtk_widget_get_ancestor (data, MOUSEPAD_TYPE_WINDOW));
+  mousepad_window_externally_modified (document->file, window);
 
   return FALSE;
 }
@@ -2882,7 +2877,8 @@ mousepad_window_pending_window (MousepadWindow *window)
 
   /* try again to inform the user about this file whenever idle, to allow for a possible
    * closing process to take place before */
-  g_idle_add (mousepad_window_pending_widget_idle, window->active);
+  g_idle_add (mousepad_window_pending_widget_idle,
+              mousepad_util_source_autoremove (window->active));
 }
 
 
@@ -2900,7 +2896,7 @@ mousepad_window_pending_tab (GtkNotebook  *notebook,
 
       /* try again to inform the user about this file whenever idle, to allow for a possible
        * closing process to take place before */
-      g_idle_add (mousepad_window_pending_widget_idle, page);
+      g_idle_add (mousepad_window_pending_widget_idle, mousepad_util_source_autoremove (page));
     }
 }
 
@@ -4185,7 +4181,8 @@ mousepad_window_search_completed (MousepadWindow      *window,
 
   /* make sure the selection is visible whenever idle */
   if (n_matches_doc > 0)
-    g_idle_add (mousepad_view_scroll_to_cursor, window->active->textview);
+    g_idle_add (mousepad_view_scroll_to_cursor,
+                mousepad_util_source_autoremove (window->active->textview));
 }
 
 
@@ -4955,7 +4952,8 @@ mousepad_window_action_reload (GSimpleAction *action,
       g_error_free (error);
     }
   else
-    g_idle_add (mousepad_view_scroll_to_cursor, window->active->textview);
+    g_idle_add (mousepad_view_scroll_to_cursor,
+                mousepad_util_source_autoremove (window->active->textview));
 }
 
 

mousepad/mousepad-dialogs.c
@@ -619,9 +619,7 @@ mousepad_dialogs_combo_insert_separator (GtkTreeModel *model,
 static gboolean
 mousepad_dialogs_combo_popup (gpointer data)
 {
-  /* the combo box may no longer exist when we get here */
-  if (GTK_IS_COMBO_BOX (data))
-    gtk_combo_box_popup (GTK_COMBO_BOX (data));
+  gtk_combo_box_popup (data);
 
   return FALSE;
 }
@@ -792,7 +790,8 @@ mousepad_dialogs_combo_changed (GtkComboBox *combo,
         }
 
       /* reopen combo box */
-      g_idle_add_full (G_PRIORITY_LOW, mousepad_dialogs_combo_popup, combo, NULL);
+      g_idle_add_full (G_PRIORITY_LOW, mousepad_dialogs_combo_popup,
+                       mousepad_util_source_autoremove (combo), NULL);
     }
 }
 

mousepad/mousepad-file.c
@@ -213,37 +213,32 @@ mousepad_file_monitor_changed (GFileMonitor      *monitor,
 static gboolean
 mousepad_file_set_monitor (gpointer data)
 {
-  MousepadFile *file;
+  MousepadFile *file = data;
   GError       *error = NULL;
 
-  /* the file may have been released during the delay */
-  if (MOUSEPAD_IS_FILE (data))
+  if (G_IS_FILE_MONITOR (file->monitor))
+    g_object_unref (file->monitor);
+
+  if (MOUSEPAD_SETTING_GET_BOOLEAN (MONITOR_CHANGES))
     {
-      file = MOUSEPAD_FILE (data);
-      if (G_IS_FILE_MONITOR (file->monitor))
-        g_object_unref (file->monitor);
-
-      if (MOUSEPAD_SETTING_GET_BOOLEAN (MONITOR_CHANGES))
-        {
-          file->monitor = g_file_monitor_file (file->location,
+      file->monitor = g_file_monitor_file (file->location,
 #if GLIB_CHECK_VERSION (2, 56, 2)
-                                               G_FILE_MONITOR_WATCH_HARD_LINKS |
+                                           G_FILE_MONITOR_WATCH_HARD_LINKS |
 #endif
-                                               G_FILE_MONITOR_WATCH_MOVES,
-                                               NULL, &error);
+                                           G_FILE_MONITOR_WATCH_MOVES,
+                                           NULL, &error);
 
-          /* inform the user */
-          if (error != NULL)
-            {
-              g_message ("File monitoring is disabled for file '%s': %s",
-                         mousepad_file_get_path (file), error->message);
-              g_error_free (error);
-            }
-          /* watch file for changes */
-          else
-            g_signal_connect (file->monitor, "changed",
-                              G_CALLBACK (mousepad_file_monitor_changed), file);
+      /* inform the user */
+      if (error != NULL)
+        {
+          g_message ("File monitoring is disabled for file '%s': %s",
+                     mousepad_file_get_path (file), error->message);
+          g_error_free (error);
         }
+      /* watch file for changes */
+      else
+        g_signal_connect (file->monitor, "changed",
+                          G_CALLBACK (mousepad_file_monitor_changed), file);
     }
 
   return FALSE;
@@ -284,7 +279,7 @@ mousepad_file_set_location (MousepadFile *file,
       /* activate file monitoring with a delay, to not consider our own saving as
        * external modification after a save as */
       g_timeout_add (MOUSEPAD_SETTING_GET_UINT (MONITOR_DISABLING_TIMER),
-                     mousepad_file_set_monitor, file);
+                     mousepad_file_set_monitor, mousepad_util_source_autoremove (file));
 
       /* send a signal that the name has been changed */
       g_signal_emit (file, file_signals[LOCATION_CHANGED], 0, file->location);
@@ -720,15 +715,9 @@ mousepad_file_open (MousepadFile  *file,
 static gboolean
 mousepad_file_monitor_unblock (gpointer data)
 {
-  MousepadFile *file;
+  MousepadFile *file = data;
 
-  /* the file or its monitor may have been released during the delay */
-  if (MOUSEPAD_IS_FILE (data))
-    {
-      file = MOUSEPAD_FILE (data);
-      if (G_IS_FILE_MONITOR (file->monitor))
-        g_signal_handlers_unblock_by_func (file->monitor, mousepad_file_monitor_changed, data);
-    }
+  g_signal_handlers_unblock_by_func (file->monitor, mousepad_file_monitor_changed, data);
 
   return FALSE;
 }
@@ -761,7 +750,7 @@ mousepad_file_replace_contents (MousepadFile      *m_file,
    * external modification */
   if (G_IS_FILE_MONITOR (m_file->monitor))
     g_timeout_add (MOUSEPAD_SETTING_GET_UINT (MONITOR_DISABLING_TIMER),
-                   mousepad_file_monitor_unblock, m_file);
+                   mousepad_file_monitor_unblock, mousepad_util_source_autoremove (m_file));
 
   return succeed;
 }

mousepad/mousepad-prefs-dialog.c
@@ -433,10 +433,6 @@ mousepad_prefs_dialog_checkbox_toggled_idle (gpointer data)
   GtkWidget              *button = data, *box, *popover;
   gboolean                visible;
 
-  /* the button could have been destroyed when we get here */
-  if (! GTK_IS_BUTTON (button))
-    return FALSE;
-
   provider = mousepad_object_get_data (button, "provider");
   box = mousepad_plugin_provider_get_setting_box (provider);
   visible = gtk_widget_get_visible (button);
@@ -467,7 +463,8 @@ mousepad_prefs_dialog_checkbox_toggled (GtkToggleButton *checkbox,
                                         GtkWidget       *button)
 {
   /* wait for the plugin to get its setting box, or for this box to be destroyed */
-  g_idle_add (mousepad_prefs_dialog_checkbox_toggled_idle, button);
+  g_idle_add (mousepad_prefs_dialog_checkbox_toggled_idle,
+              mousepad_util_source_autoremove (button));
 }
 
 

mousepad/mousepad-util.c
@@ -1185,3 +1185,30 @@ G_GNUC_END_IGNORE_DEPRECATIONS
 
 #endif
 }
+
+
+
+static void
+mousepad_util_source_remove_all (gpointer  data,
+                                 GObject  *object)
+{
+  while (g_source_remove_by_user_data (object));
+}
+
+
+
+/* an often sufficient way to automate memory management of sources, without having
+ * to store a source id and use an ad hoc handler */
+gpointer
+mousepad_util_source_autoremove (gpointer object)
+{
+  g_return_val_if_fail (G_IS_OBJECT (object), object);
+
+  if (! mousepad_object_get_data (object, "source-autoremove"))
+    {
+      g_object_weak_ref (object, mousepad_util_source_remove_all, NULL);
+      mousepad_object_set_data (object, "source-autoremove", GINT_TO_POINTER (TRUE));
+    }
+
+  return object;
+}

mousepad/mousepad-util.h
@@ -98,6 +98,8 @@ gchar       *mousepad_util_pango_font_description_to_css    (const PangoFontDesc
 
 const gchar *mousepad_util_get_path                         (GFile                      *file);
 
+gpointer     mousepad_util_source_autoremove                (gpointer                    object);
+
 G_END_DECLS
 
 #endif /* !__MOUSEPAD_UTIL_H__ */

mousepad/mousepad-view.c
@@ -671,18 +671,13 @@ mousepad_view_scroll_to_cursor (gpointer data)
   MousepadView  *view = data;
   GtkTextBuffer *buffer;
 
-  /* if there is a delayed request to scroll to cursor just before closing the window
-   * or a tab, this test could fail */
-  if (MOUSEPAD_IS_VIEW (view))
-    {
-      /* get the buffer */
-      buffer = mousepad_view_get_buffer (view);
+  /* get the buffer */
+  buffer = mousepad_view_get_buffer (view);
 
-      /* scroll to visible area */
-      gtk_text_view_scroll_to_mark (GTK_TEXT_VIEW (view),
-                                    gtk_text_buffer_get_insert (buffer),
-                                    0.02, FALSE, 0.0, 0.0);
-    }
+  /* scroll to visible area */
+  gtk_text_view_scroll_to_mark (GTK_TEXT_VIEW (view),
+                                gtk_text_buffer_get_insert (buffer),
+                                0.02, FALSE, 0.0, 0.0);
 
   return FALSE;
 }

commit 068015b526785630faca4415d3e582f729b715cc

Add a sanity check on the window for "Externally Modified"
A regression introduced in c5200610bad2a34f940e1fdbb1f0d70c104e0537:
although it is unlikely to happen, the window can be NULL here, only the
document is guaranteed to exist.

mousepad/mousepad-window.c
@@ -2857,7 +2857,8 @@ mousepad_window_pending_widget_idle (gpointer data)
   MousepadWindow   *window;
 
   window = MOUSEPAD_WINDOW (gtk_widget_get_ancestor (data, MOUSEPAD_TYPE_WINDOW));
-  mousepad_window_externally_modified (document->file, window);
+  if (window != NULL)
+    mousepad_window_externally_modified (document->file, window);
 
   return FALSE;
 }