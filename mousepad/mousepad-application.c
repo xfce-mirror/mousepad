xfce-mirror/mousepad

commit 8349795ed7b5ee2f4ab42ff3466a47e4dc035dbe

Code refactoring around GAction-GSettings binding
Using the correspondence between GAction and GSettings (common names,
common use of GVariant), this commit globally simplifies action
handling. In particular, setting-related actions are transferred from
the window level to the application level, resulting in only three
generic handlers to bind an action to its setting.

As `mousepad/resources/ui/mousepad-prefs-dialog.ui` was already heavily
impacted by these changes, this also adds some cleaning (consistency
and/or updating of the notations used), and incorporates some object
property bindings currently made in `mousepad/mousepad-prefs-dialog.c`.

See !48 for more details.

mousepad/mousepad-application.c
@@ -54,6 +54,15 @@ static void        mousepad_application_create_style_schemes_menu (MousepadAppli
 static void        mousepad_application_action_quit               (GSimpleAction            *action,
                                                                    GVariant                 *value,
                                                                    gpointer                  data);
+static void        mousepad_application_toggle_activate           (GSimpleAction            *action,
+                                                                   GVariant                 *parameter,
+                                                                   gpointer                  data);
+static void        mousepad_application_radio_activate            (GSimpleAction            *action,
+                                                                   GVariant                 *parameter,
+                                                                   gpointer                  data);
+static void        mousepad_application_action_update             (MousepadApplication      *application,
+                                                                   gchar                    *key,
+                                                                   GSettings                *settings);
 
 
 
@@ -101,16 +110,6 @@ static const GOptionEntry option_entries[] =
   { NULL }
 };
 
-
-
-/* application actions */
-static const GActionEntry action_entries[] =
-{
-  { "quit", mousepad_application_action_quit, NULL, NULL, NULL }
-};
-
-
-
 /* opening mode provided on the command line */
 enum
 {
@@ -118,6 +117,48 @@ enum
   WINDOW,
   MIXED
 };
+
+
+
+/* application actions */
+
+/* stateless actions */
+static const GActionEntry stateless_actions[] =
+{
+  /* command line options */
+  { "quit", mousepad_application_action_quit, NULL, NULL, NULL }
+};
+#define N_STATELESS G_N_ELEMENTS (stateless_actions)
+
+/* preferences dialog */
+static const GActionEntry setting_actions[] =
+{
+  /* "View" tab */
+  { MOUSEPAD_SETTING_SHOW_LINE_NUMBERS, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_SHOW_WHITESPACE, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_SHOW_LINE_ENDINGS, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_SHOW_RIGHT_MARGIN, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_HIGHLIGHT_CURRENT_LINE, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_MATCH_BRACES, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_WORD_WRAP, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_USE_DEFAULT_FONT, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_COLOR_SCHEME, mousepad_application_radio_activate, "s", "'none'", NULL },
+
+  /* "Editor" tab */
+  { MOUSEPAD_SETTING_INSERT_SPACES, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_AUTO_INDENT, mousepad_application_toggle_activate, NULL, "false", NULL },
+
+  /* "Window" tab */
+  { MOUSEPAD_SETTING_STATUSBAR_VISIBLE, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_PATH_IN_TITLE, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_REMEMBER_SIZE, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_REMEMBER_POSITION, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_REMEMBER_STATE, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_TOOLBAR_VISIBLE, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_ALWAYS_SHOW_TABS, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_CYCLE_TABS, mousepad_application_toggle_activate, NULL, "false", NULL }
+};
+#define N_SETTING G_N_ELEMENTS (setting_actions)
 
 
 
@@ -348,7 +389,7 @@ mousepad_application_set_accels (MousepadApplication *application)
     { "win.search.go-to", "<Control>L" },
 
     /* "View" menu */
-    { "win.view.menubar", "<Control>M" }, { "win.view.fullscreen", "F11" },
+    { "win.preferences.window.menubar-visible", "<Control>M" }, { "win.view.fullscreen", "F11" },
 
     /* "Document" menu */
     { "win.document.previous-tab", "<Control>Page_Up" }, { "win.document.next-tab", "<Control>Page_Down" },
@@ -404,6 +445,8 @@ static void
 mousepad_application_startup (GApplication *gapplication)
 {
   MousepadApplication *application = MOUSEPAD_APPLICATION (gapplication);
+  GVariant            *state;
+  guint                n;
 
   /* chain up to parent */
   G_APPLICATION_CLASS (mousepad_application_parent_class)->startup (gapplication);
@@ -416,8 +459,23 @@ mousepad_application_startup (GApplication *gapplication)
   mousepad_settings_init ();
 
   /* add application actions */
-  g_action_map_add_action_entries (G_ACTION_MAP (application), action_entries,
-                                   G_N_ELEMENTS (action_entries), application);
+  g_action_map_add_action_entries (G_ACTION_MAP (application), stateless_actions,
+                                   N_STATELESS, application);
+  g_action_map_add_action_entries (G_ACTION_MAP (application), setting_actions,
+                                   N_SETTING, application);
+
+  for (n = 0; n < N_SETTING; n++)
+    {
+      /* sync the action state to its setting */
+      mousepad_setting_connect_object (setting_actions[n].name,
+                                       G_CALLBACK (mousepad_application_action_update),
+                                       application, G_CONNECT_SWAPPED);
+
+      /* initialize the action state */
+      state = mousepad_setting_get_variant (setting_actions[n].name);
+      g_action_group_change_action_state (G_ACTION_GROUP (application), setting_actions[n].name, state);
+      g_variant_unref (state);
+    }
 
   /* set accels for actions */
   mousepad_application_set_accels (application);
@@ -917,7 +975,7 @@ mousepad_application_create_style_schemes_menu (MousepadApplication *application
     {
       /* create menu item */
       label = gtk_source_style_scheme_get_id (iter->data);
-      action_name = g_strconcat ("win.view.color-scheme('", label, "')", NULL);
+      action_name = g_strconcat ("app.preferences.view.color-scheme('", label, "')", NULL);
       label = gtk_source_style_scheme_get_name (iter->data);
       item = g_menu_item_new (label, action_name);
 
@@ -951,4 +1009,53 @@ mousepad_application_action_quit (GSimpleAction *action,
                                   gpointer       data)
 {
   g_application_quit (G_APPLICATION (data));
+}
+
+
+
+static void
+mousepad_application_toggle_activate (GSimpleAction *action,
+                                      GVariant      *parameter,
+                                      gpointer       data)
+{
+  gboolean state;
+
+  /* save the setting */
+  state = ! g_variant_get_boolean (g_action_get_state (G_ACTION (action)));
+  mousepad_setting_set_boolean (g_action_get_name (G_ACTION (action)), state);
+}
+
+
+
+static void
+mousepad_application_radio_activate (GSimpleAction *action,
+                                     GVariant      *parameter,
+                                     gpointer       data)
+{
+  /* save the setting */
+  mousepad_setting_set_variant (g_action_get_name (G_ACTION (action)), parameter);
+}
+
+
+
+static void
+mousepad_application_action_update (MousepadApplication *application,
+                                    gchar               *key,
+                                    GSettings           *settings)
+{
+  GVariant *state;
+  gchar    *schema_id, *action_name;
+
+  /* retrieve the action name from the setting schema id */
+  g_object_get (settings, "schema_id", &schema_id, NULL);
+  action_name = g_strdup_printf ("%s.%s", schema_id + MOUSEPAD_ID_LEN + 1, key);
+
+  /* request for the action state to be changed according to the setting */
+  state = mousepad_setting_get_variant (action_name);
+  g_action_group_change_action_state (G_ACTION_GROUP (application), action_name, state);
+
+  /* cleanup */
+  g_free (schema_id);
+  g_free (action_name);
+  g_variant_unref (state);
 }

mousepad/mousepad-prefs-dialog.c
@@ -26,40 +26,20 @@
 #define WID_NOTEBOOK                        "/prefs/main-notebook"
 
 /* View page */
-#define WID_SHOW_LINE_NUMBERS_CHECK         "/prefs/view/display/show-line-numbers-check"
-#define WID_SHOW_WHITESPACE_CHECK           "/prefs/view/display/display-whitespace-check"
-#define WID_SHOW_LINE_ENDINGS_CHECK         "/prefs/view/display/display-line-endings-check"
-#define WID_SHOW_RIGHT_MARGIN_CHECK         "/prefs/view/display/long-line-check"
 #define WID_RIGHT_MARGIN_SPIN               "/prefs/view/display/long-line-spin"
-#define WID_HIGHLIGHT_CURRENT_LINE_CHECK    "/prefs/view/display/highlight-current-line-check"
-#define WID_MATCH_BRACES_CHECK              "/prefs/view/display/match-braces-check"
-#define WID_WORD_WRAP_CHECK                 "/prefs/view/display/word-wrap-check"
-
-#define WID_USE_DEFAULT_FONT_CHECK          "/prefs/view/font/default-check"
 #define WID_FONT_BUTTON                     "/prefs/view/font/chooser-button"
-#define WID_SCHEME_COMBO                    "/prefs/view/color-scheme-combo"
-#define WID_SCHEME_MODEL                    "/prefs/view/color-scheme-model"
+#define WID_SCHEME_COMBO                    "/prefs/view/color-scheme/combo"
+#define WID_SCHEME_MODEL                    "/prefs/view/color-scheme/model"
 
 /* Editor page */
-#define WID_TAB_WIDTH_SPIN                  "/prefs/editor/tab-width-spin"
-#define WID_TAB_MODE_COMBO                  "/prefs/editor/tab-mode-combo"
-#define WID_AUTO_INDENT_CHECK               "/prefs/editor/auto-indent-check"
-#define WID_SMART_HOME_END_COMBO            "/prefs/editor/smart-home-end-combo"
+#define WID_TAB_WIDTH_SPIN                  "/prefs/editor/indentation/tab-width-spin"
+#define WID_TAB_MODE_COMBO                  "/prefs/editor/indentation/tab-mode-combo"
+#define WID_SMART_HOME_END_COMBO            "/prefs/editor/home-end/smart-home-end-combo"
 
 /* Window page */
-#define WID_STATUSBAR_VISIBLE_CHECK         "/prefs/window/general/show-statusbar-check"
-#define WID_PATH_IN_TITLE_CHECK             "/prefs/window/general/show-path-in-title-check"
-#define WID_REMEMBER_SIZE_CHECK             "/prefs/window/general/remember-window-size-check"
-#define WID_REMEMBER_POSITION_CHECK         "/prefs/window/general/remember-window-position-check"
-#define WID_REMEMBER_STATE_CHECK            "/prefs/window/general/remember-window-state-check"
-#define WID_ALWAYS_SHOW_TABS_CHECK          "/prefs/window/notebook/always-show-tabs-check"
-#define WID_CYCLE_TABS_CHECK                "/prefs/window/notebook/cycle-tabs-check"
-#define WID_OPENING_MODE_COMBO              "/prefs/window/notebook/opening-mode-combo"
-#define WID_TOOLBAR_VISIBLE_CHECK           "/prefs/window/toolbar/visible-check"
 #define WID_TOOLBAR_STYLE_COMBO             "/prefs/window/toolbar/style-combo"
-#define WID_TOOLBAR_STYLE_LABEL             "/prefs/window/toolbar/style-label"
 #define WID_TOOLBAR_ICON_SIZE_COMBO         "/prefs/window/toolbar/icon-size-combo"
-#define WID_TOOLBAR_ICON_SIZE_LABEL         "/prefs/window/toolbar/icon-size-label"
+#define WID_OPENING_MODE_COMBO              "/prefs/window/notebook/opening-mode-combo"
 
 
 
@@ -433,11 +413,11 @@ mousepad_prefs_dialog_opening_mode_setting_changed (MousepadPrefsDialog *self,
 static void
 mousepad_prefs_dialog_init (MousepadPrefsDialog *self)
 {
-  GError *error = NULL;
+  GError    *error = NULL;
   GtkWidget *notebook;
   GtkWidget *content_area;
   GtkWidget *button;
-  GtkWidget *check, *widget;
+  GtkWidget *widget;
 
   self->builder = gtk_builder_new ();
 
@@ -450,6 +430,10 @@ mousepad_prefs_dialog_init (MousepadPrefsDialog *self)
       /* not reached */
       g_error_free (error);
     }
+
+  /* make the application actions usable in the prefs dialog */
+  gtk_widget_insert_action_group (GTK_WIDGET (self), "app",
+                                  G_ACTION_GROUP (g_application_get_default ()));
 
   /* add the Glade/GtkBuilder notebook into this dialog */
   notebook = mousepad_builder_get_widget (self->builder, WID_NOTEBOOK);
@@ -467,17 +451,6 @@ mousepad_prefs_dialog_init (MousepadPrefsDialog *self)
   gtk_window_set_title (GTK_WINDOW (self), _("Preferences"));
   gtk_window_set_icon_name (GTK_WINDOW (self), "preferences-desktop");
 
-  /* enable/disable right margin spin button when checkbox is changed */
-  check = mousepad_builder_get_widget (self->builder, WID_SHOW_RIGHT_MARGIN_CHECK);
-  widget = mousepad_builder_get_widget (self->builder, WID_RIGHT_MARGIN_SPIN);
-  g_object_bind_property (check, "active", widget, "sensitive", G_BINDING_SYNC_CREATE);
-
-  /* enable/disable font chooser button when the default font checkbox is changed */
-  check = mousepad_builder_get_widget (self->builder, WID_USE_DEFAULT_FONT_CHECK);
-  widget = mousepad_builder_get_widget (self->builder, WID_FONT_BUTTON);
-  g_object_bind_property (check, "active", widget, "sensitive",
-                          G_BINDING_SYNC_CREATE | G_BINDING_INVERT_BOOLEAN);
-
   /* setup tab mode combo box */
   widget = mousepad_builder_get_widget (self->builder, WID_TAB_MODE_COMBO);
   gtk_combo_box_set_active (GTK_COMBO_BOX (widget), MOUSEPAD_SETTING_GET_BOOLEAN (INSERT_SPACES));
@@ -486,57 +459,13 @@ mousepad_prefs_dialog_init (MousepadPrefsDialog *self)
   widget = mousepad_builder_get_widget (self->builder, WID_SMART_HOME_END_COMBO);
   gtk_combo_box_set_active (GTK_COMBO_BOX (widget), MOUSEPAD_SETTING_GET_ENUM (SMART_HOME_END));
 
-  /* enable/disable toolbar-related widgets when checkbox changes */
-  check = mousepad_builder_get_widget (self->builder, WID_TOOLBAR_VISIBLE_CHECK);
-
-  widget = mousepad_builder_get_widget (self->builder, WID_TOOLBAR_STYLE_LABEL);
-  g_object_bind_property (check, "active", widget, "sensitive", G_BINDING_SYNC_CREATE);
-  widget = mousepad_builder_get_widget (self->builder, WID_TOOLBAR_STYLE_COMBO);
-  g_object_bind_property (check, "active", widget, "sensitive", G_BINDING_SYNC_CREATE);
+  /* setup toolbar-related combo box */
   gtk_combo_box_set_active (GTK_COMBO_BOX (widget), MOUSEPAD_SETTING_GET_ENUM (TOOLBAR_STYLE));
-
-  widget = mousepad_builder_get_widget (self->builder, WID_TOOLBAR_ICON_SIZE_LABEL);
-  g_object_bind_property (check, "active", widget, "sensitive", G_BINDING_SYNC_CREATE);
-  widget = mousepad_builder_get_widget (self->builder, WID_TOOLBAR_ICON_SIZE_COMBO);
-  g_object_bind_property (check, "active", widget, "sensitive", G_BINDING_SYNC_CREATE);
   mousepad_prefs_dialog_toolbar_icon_size_setting_changed (self, NULL, NULL);
 
   /* setup opening mode combo box */
   widget = mousepad_builder_get_widget (self->builder, WID_OPENING_MODE_COMBO);
   gtk_combo_box_set_active (GTK_COMBO_BOX (widget), MOUSEPAD_SETTING_GET_ENUM (OPENING_MODE));
-
-  /* bind checkboxes to settings */
-#define BIND_CHECKBOX(setting)                                           \
-  MOUSEPAD_SETTING_BIND (setting,                                        \
-                         gtk_builder_get_object (self->builder,          \
-                                                 WID_##setting##_CHECK), \
-                         "active",                                       \
-                         G_SETTINGS_BIND_DEFAULT)
-
-  /* View */
-  BIND_CHECKBOX (SHOW_LINE_NUMBERS);
-  BIND_CHECKBOX (SHOW_WHITESPACE);
-  BIND_CHECKBOX (SHOW_LINE_ENDINGS);
-  BIND_CHECKBOX (SHOW_RIGHT_MARGIN);
-  BIND_CHECKBOX (HIGHLIGHT_CURRENT_LINE);
-  BIND_CHECKBOX (WORD_WRAP);
-  BIND_CHECKBOX (USE_DEFAULT_FONT);
-  BIND_CHECKBOX (MATCH_BRACES);
-
-  /* Editor */
-  BIND_CHECKBOX (AUTO_INDENT);
-
-  /* Window */
-  BIND_CHECKBOX (STATUSBAR_VISIBLE);
-  BIND_CHECKBOX (PATH_IN_TITLE);
-  BIND_CHECKBOX (REMEMBER_SIZE);
-  BIND_CHECKBOX (REMEMBER_POSITION);
-  BIND_CHECKBOX (REMEMBER_STATE);
-  BIND_CHECKBOX (ALWAYS_SHOW_TABS);
-  BIND_CHECKBOX (CYCLE_TABS);
-  BIND_CHECKBOX (TOOLBAR_VISIBLE);
-
-#undef BIND_CHECKBOX
 
   /* bind the right-margin-position setting to the spin button */
   MOUSEPAD_SETTING_BIND (RIGHT_MARGIN_POSITION,

mousepad/resources/gtk/menus.ui
@@ -2,7 +2,7 @@
 <interface>
   <requires lib="gtk+" version="3.20"/>
 
-  <menu id='menubar'>
+  <menu id="menubar">
 
     <submenu id="file">
       <attribute name="label" translatable="yes">_File</attribute>
@@ -322,7 +322,7 @@
             <item>
               <attribute name="label" translatable="yes">None</attribute>
               <attribute name="tooltip" translatable="yes">No style scheme</attribute>
-              <attribute name="action">win.view.color-scheme</attribute>
+              <attribute name="action">app.preferences.view.color-scheme</attribute>
               <attribute name="target" type="s">'none'</attribute>
             </item>
           </section>
@@ -332,24 +332,24 @@
         <item>
           <attribute name="label" translatable="yes">Line N_umbers</attribute>
           <attribute name="tooltip" translatable="yes">Show line numbers</attribute>
-          <attribute name="action">win.view.line-numbers</attribute>
+          <attribute name="action">app.preferences.view.show-line-numbers</attribute>
         </item>
       </section>
       <section>
         <item>
           <attribute name="label" translatable="yes">_Menubar</attribute>
           <attribute name="tooltip" translatable="yes">Change the visibility of the main menubar</attribute>
-          <attribute name="action">win.view.menubar</attribute>
+          <attribute name="action">win.preferences.window.menubar-visible</attribute>
         </item>
         <item>
           <attribute name="label" translatable="yes">_Toolbar</attribute>
           <attribute name="tooltip" translatable="yes">Change the visibility of the toolbar</attribute>
-          <attribute name="action">win.view.toolbar</attribute>
+          <attribute name="action">win.preferences.window.toolbar-visible</attribute>
         </item>
         <item>
           <attribute name="label" translatable="yes">St_atusbar</attribute>
           <attribute name="tooltip" translatable="yes">Change the visibility of the statusbar</attribute>
-          <attribute name="action">win.view.statusbar</attribute>
+          <attribute name="action">win.preferences.window.statusbar-visible</attribute>
         </item>
       </section>
       <section id="view.fullscreen">
@@ -370,7 +370,7 @@
         <item>
           <attribute name="label" translatable="yes">_Word Wrap</attribute>
           <attribute name="tooltip" translatable="yes">Toggle breaking lines in between words</attribute>
-          <attribute name="action">win.document.word-wrap</attribute>
+          <attribute name="action">app.preferences.view.word-wrap</attribute>
         </item>
         <item>
           <attribute name="label" translatable="yes">_Auto Indent</attribute>
@@ -416,7 +416,7 @@
             <item>
               <attribute name="label" translatable="yes">Insert _Spaces</attribute>
               <attribute name="tooltip" translatable="yes">Insert spaces when the tab button is pressed</attribute>
-              <attribute name="action">win.document.tab.insert-spaces</attribute>
+              <attribute name="action">app.preferences.view.insert-spaces</attribute>
             </item>
           </section>
         </submenu>
@@ -505,7 +505,7 @@
 
 
 
-  <menu id='tab-menu'>
+  <menu id="tab-menu">
 
     <section>
       <item>
@@ -549,7 +549,7 @@
 
 
 
-  <menu id='textview-menu'>
+  <menu id="textview-menu">
 
     <section>
       <item>

commit 44946a64b1a1a48412b3618ab82c13b76b170dcc

Fix GVariant-related memory leaks
Mainly introduced in 8349795ed7b5ee2f4ab42ff3466a47e4dc035dbe (!48).

mousepad/mousepad-application.c
@@ -1591,7 +1591,7 @@ mousepad_application_toggle_activate (GSimpleAction *action,
   gboolean state;
 
   /* save the setting */
-  state = ! g_variant_get_boolean (g_action_get_state (G_ACTION (action)));
+  state = ! mousepad_action_get_state_boolean (G_ACTION (action));
   mousepad_setting_set_boolean (g_action_get_name (G_ACTION (action)), state);
 }
 
@@ -1619,7 +1619,7 @@ mousepad_application_plugin_activate (GSimpleAction *action,
   guint         length = 0;
 
   /* get the new action state */
-  enabled = ! g_variant_get_boolean (g_action_get_state (G_ACTION (action)));
+  enabled = ! mousepad_action_get_state_boolean (G_ACTION (action));
 
   /* get the list of enabled plugins */
   plugins = MOUSEPAD_SETTING_GET_STRV (ENABLED_PLUGINS);
@@ -1687,7 +1687,7 @@ mousepad_application_plugin_update (MousepadApplication *application)
 {
   MousepadPluginProvider  *mprovider;
   GTypeModule             *provider;
-  GActionGroup            *group = G_ACTION_GROUP (application);
+  GAction                 *action;
   GList                   *item;
   gchar                  **plugins;
   gboolean                 enabled, contained, destroyable;
@@ -1701,12 +1701,11 @@ mousepad_application_plugin_update (MousepadApplication *application)
     {
       provider = item->data;
       contained = g_strv_contains ((const gchar *const *) plugins, provider->name);
-      enabled = g_variant_get_boolean (g_action_group_get_action_state (group, provider->name));
+      action = g_action_map_lookup_action (G_ACTION_MAP (application), provider->name);
+      enabled = mousepad_action_get_state_boolean (action);
       if ((enabled && ! contained) || (! enabled && contained))
         {
-          g_action_group_change_action_state (group, provider->name,
-                                              g_variant_new_boolean (! enabled));
-
+          g_action_change_state (action, g_variant_new_boolean (! enabled));
           mprovider = item->data;
           destroyable = mousepad_plugin_provider_is_destroyable (mprovider);
 

mousepad/mousepad-application.h
@@ -21,6 +21,20 @@
 
 G_BEGIN_DECLS
 
+/* repetitive code whose forgetting causes memory leaks */
+static inline gboolean
+mousepad_action_get_state_boolean (GAction *action)
+{
+  GVariant *variant;
+  gboolean  value;
+
+  variant = g_action_get_state (action);
+  value = g_variant_get_boolean (variant);
+  g_variant_unref (variant);
+
+  return value;
+}
+
 typedef struct _MousepadApplicationClass MousepadApplicationClass;
 typedef struct _MousepadApplication      MousepadApplication;
 

mousepad/mousepad-window.c
@@ -5839,7 +5839,7 @@ mousepad_window_action_bar_activate (GSimpleAction *action,
 
   g_return_if_fail (MOUSEPAD_IS_WINDOW (data));
 
-  state = ! g_variant_get_boolean (g_action_get_state (G_ACTION (action)));
+  state = ! mousepad_action_get_state_boolean (G_ACTION (action));
   mousepad_setting_set_boolean (g_action_get_name (G_ACTION (action)), state);
 }
 
@@ -5876,14 +5876,14 @@ mousepad_window_action_menubar_state (GSimpleAction *action,
   g_return_if_fail (MOUSEPAD_IS_WINDOW (window));
 
   /* exit if there is actually no change */
-  if (g_variant_equal (state, g_action_get_state (G_ACTION (action))))
+  visible = g_variant_get_boolean (state);
+  if (visible == mousepad_action_get_state_boolean (G_ACTION (action)))
     return;
 
   /* set the action state */
   g_simple_action_set_state (action, state);
 
   /* show/hide the "Menubar" item in the text view menu */
-  visible = g_variant_get_boolean (state);
   textview_action = g_action_map_lookup_action (G_ACTION_MAP (window), "textview.menubar");
   g_simple_action_set_enabled (G_SIMPLE_ACTION (textview_action), ! visible);
 
@@ -5931,7 +5931,7 @@ mousepad_window_action_fullscreen (GSimpleAction *action,
   MousepadWindow *window = MOUSEPAD_WINDOW (data);
   gboolean        fullscreen;
 
-  fullscreen = ! g_variant_get_boolean (g_action_get_state (G_ACTION (action)));
+  fullscreen = ! mousepad_action_get_state_boolean (G_ACTION (action));
   g_action_change_state (G_ACTION (action), g_variant_new_boolean (fullscreen));
 
   /* entering/leaving fullscreen mode */
@@ -6058,7 +6058,7 @@ mousepad_window_action_write_bom (GSimpleAction *action,
       lock_menu_updates++;
 
       /* set the current state */
-      state = ! g_variant_get_boolean (g_action_get_state (G_ACTION (action)));
+      state = ! mousepad_action_get_state_boolean (G_ACTION (action));
       g_action_change_state (G_ACTION (action), g_variant_new_boolean (state));
 
       /* set new value */
@@ -6092,7 +6092,7 @@ mousepad_window_action_viewer_mode (GSimpleAction *action,
       lock_menu_updates++;
 
       /* set the current state */
-      state = ! g_variant_get_boolean (g_action_get_state (G_ACTION (action)));
+      state = ! mousepad_action_get_state_boolean (G_ACTION (action));
       g_action_change_state (G_ACTION (action), g_variant_new_boolean (state));
 
       /* set new value */