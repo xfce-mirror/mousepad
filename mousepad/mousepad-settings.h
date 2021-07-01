597f7ad343e2f0b8fcaceea63c993742bae4cbfa
Gaël Bonithon <gael@xfce.org>
2021-07-01 15:15:09 +0200

    Add autosave support

--- mousepad/mousepad-settings.h
+++ mousepad/mousepad-settings.h
@@ -28,6 +28,7 @@ G_BEGIN_DECLS
 #define MOUSEPAD_SETTING_MONITOR_CHANGES              "preferences.file.monitor-changes"
 #define MOUSEPAD_SETTING_MONITOR_DISABLING_TIMER      "preferences.file.monitor-disabling-timer"
 #define MOUSEPAD_SETTING_REMEMBER_SESSION             "preferences.file.remember-session"
+#define MOUSEPAD_SETTING_AUTOSAVE_TIMER               "preferences.file.autosave-timer"
 
 #define MOUSEPAD_SETTING_AUTO_INDENT                  "preferences.view.auto-indent"
 #define MOUSEPAD_SETTING_FONT                         "preferences.view.font-name"
