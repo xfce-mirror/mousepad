9501ed48b78c049dfef720486b429ac06d090a58
Gaël Bonithon <gael@xfce.org>
2021-07-03 10:07:53 +0200

    Adapt opening process to include autosaved files

--- mousepad/mousepad-file.c
+++ mousepad/mousepad-file.c
@@ -280,7 +280,23 @@ mousepad_file_set_location (MousepadFile *file,
                             GFile        *location,
                             gboolean      real)
 {
+  GFile       *autosave_location;
+  const gchar *autosave_uri;
+
   g_return_if_fail (MOUSEPAD_IS_FILE (file));
+
+  /* autosave restore: some preliminaries */
+  if (location != NULL
+      && (autosave_uri = mousepad_object_get_data (location, "autosave-uri")) != NULL)
+    {
+      /* update autosave location */
+      autosave_location = g_file_new_for_uri (autosave_uri);
+      g_object_unref (file->autosave_location);
+      file->autosave_location = autosave_location;
+
+      /* mark location as temporary */
+      real = FALSE;
+    }
 
   /* set location */
   if (file->location == NULL && location != NULL)
@@ -579,6 +595,7 @@ mousepad_file_open (MousepadFile  *file,
 {
   MousepadEncoding  bom_encoding;
   GtkTextIter       start, end;
+  GFile            *location = file->location;
   GFileInfo        *fileinfo;
   const gchar      *charset, *bom_charset, *endc, *n, *m;
   gchar            *contents = NULL, *etag, *temp;
@@ -587,11 +604,15 @@ mousepad_file_open (MousepadFile  *file,
 
   g_return_val_if_fail (MOUSEPAD_IS_FILE (file), FALSE);
   g_return_val_if_fail (GTK_IS_TEXT_BUFFER (file->buffer), FALSE);
-  g_return_val_if_fail (file->location != NULL, FALSE);
+  g_return_val_if_fail (location != NULL, FALSE);
   g_return_val_if_fail (error == NULL || *error == NULL, FALSE);
 
+  /* autosave restore */
+  if (mousepad_object_get_data (location, "autosave-uri") != NULL)
+    location = file->autosave_location;
+
   /* if the file does not exist and this is allowed, no problem */
-  if (! g_file_load_contents (file->location, NULL, &contents, &file_size, &etag, error)
+  if (! g_file_load_contents (location, NULL, &contents, &file_size, &etag, error)
       && g_error_matches (*error, G_IO_ERROR, G_IO_ERROR_NOT_FOUND) && ! must_exist)
     {
       g_clear_error (error);
@@ -742,7 +763,7 @@ mousepad_file_open (MousepadFile  *file,
 
       /* store the file status */
       if (G_LIKELY (! file->temporary))
-        if (G_LIKELY (fileinfo = g_file_query_info (file->location, G_FILE_ATTRIBUTE_ACCESS_CAN_WRITE,
+        if (G_LIKELY (fileinfo = g_file_query_info (location, G_FILE_ATTRIBUTE_ACCESS_CAN_WRITE,
                                                     G_FILE_QUERY_INFO_NONE, NULL, error)))
           {
             mousepad_file_set_read_only (file,
