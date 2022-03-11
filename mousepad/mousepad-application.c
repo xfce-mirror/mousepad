xfce-mirror/mousepad

commit 28c1d32582b554ffee06c5b9c6892724a9b53a06

Various small improvements regarding accels
* Fix a potential memory leak when disabling editing accels (related to
  !66)
* Exclude more useless actions from `accels.scm` (related to !59)
* Specify the filter for adding accels to the application (related to
  !59)

mousepad/mousepad-application.c
@@ -448,8 +448,9 @@ mousepad_application_update_accels (GtkApplication  *application,
   const gchar *accels[] = { NULL, NULL };
 
   /* make sure to not crash because of corrupted data */
-  if (g_str_has_prefix (accel_path, "<Actions>/") && g_strcmp0 (accel_path, "<Actions>/") != 0
-      && g_action_parse_detailed_name (accel_path + 10, &action, &target, NULL))
+  if (g_str_has_prefix (accel_path, "<Actions>/")
+      && g_action_parse_detailed_name (accel_path + 10, &action, &target, NULL)
+      && g_action_name_is_valid (accel_path + 10))
     {
       accel = gtk_accelerator_name (accel_key, accel_mods);
       if (*accel != '\0')
@@ -474,7 +475,8 @@ mousepad_application_complete_accel_map (GtkApplication *application)
   gchar        *accel_path, *filename;
   guint         n;
   gchar       **action_names;
-  const gchar  *excluded_actions[] = { "win.insensitive" };
+  const gchar  *excluded_actions[] = { "win.insensitive", "win.file.new-from-template",
+                                       "win.file.open-recent", "win.document" };
 
   /* disconnect this handler */
   mousepad_disconnect_by_func (application, mousepad_application_complete_accel_map, NULL);

mousepad/mousepad-replace-dialog.c
@@ -211,8 +211,9 @@ mousepad_replace_dialog_post_init (MousepadReplaceDialog *dialog)
       gtk_binding_entry_remove (binding_set, accel_key, accel_mods);
       gtk_binding_entry_add_signal (binding_set, accel_key, accel_mods, "delete-from-cursor",
                                     2, GTK_TYPE_DELETE_TYPE, GTK_DELETE_CHARS, G_TYPE_INT, 1);
-      g_strfreev (accels);
     }
+
+  g_strfreev (accels);
 
   for (n = 0; n < G_N_ELEMENTS (actions); n++)
     {
@@ -222,8 +223,9 @@ mousepad_replace_dialog_post_init (MousepadReplaceDialog *dialog)
           gtk_accelerator_parse (accels[0], &accel_key, &accel_mods);
           gtk_binding_entry_remove (binding_set, accel_key, accel_mods);
           gtk_binding_entry_add_signal (binding_set, accel_key, accel_mods, signals[n], 0);
-          g_strfreev (accels);
         }
+
+      g_strfreev (accels);
     }
 
   /* give the dialog its definite size by setting a fake occurrences label */

mousepad/mousepad-search-bar.c
@@ -184,8 +184,9 @@ mousepad_search_bar_post_init (MousepadSearchBar *bar)
       gtk_binding_entry_remove (binding_set, accel_key, accel_mods);
       gtk_binding_entry_add_signal (binding_set, accel_key, accel_mods, "delete-from-cursor",
                                     2, GTK_TYPE_DELETE_TYPE, GTK_DELETE_CHARS, G_TYPE_INT, 1);
-      g_strfreev (accels);
     }
+
+  g_strfreev (accels);
 
   for (n = 0; n < G_N_ELEMENTS (actions); n++)
     {
@@ -195,8 +196,9 @@ mousepad_search_bar_post_init (MousepadSearchBar *bar)
           gtk_accelerator_parse (accels[0], &accel_key, &accel_mods);
           gtk_binding_entry_remove (binding_set, accel_key, accel_mods);
           gtk_binding_entry_add_signal (binding_set, accel_key, accel_mods, signals[n], 0);
-          g_strfreev (accels);
         }
+
+      g_strfreev (accels);
     }
 }
 

commit 73a889dc86395d553905476ebd7a99a31df62111

Filter entries from `accels.scm` on non-detailed action name
A small regression introduced in 28c1d325, which prevented specifying
action names with target.

Related to !59.

mousepad/mousepad-application.c
@@ -524,7 +524,7 @@ mousepad_application_update_accels (GtkApplication  *application,
   /* make sure to not crash because of corrupted data */
   if (g_str_has_prefix (accel_path, "<Actions>/")
       && g_action_parse_detailed_name (accel_path + 10, &action, &target, NULL)
-      && g_action_name_is_valid (accel_path + 10))
+      && g_action_name_is_valid (action))
     {
       accel = gtk_accelerator_name (accel_key, accel_mods);
       if (*accel != '\0')