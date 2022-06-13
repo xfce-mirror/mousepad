xfce-mirror/mousepad

commit ea239c3f7e3b75cc75470576c6afbcafc157a302

Search history: Update on explicit search or replace

mousepad/mousepad-replace-dialog.c
@@ -24,7 +24,6 @@
 
 
 
-static void              mousepad_replace_dialog_unrealize              (GtkWidget             *widget);
 static void              mousepad_replace_dialog_finalize               (GObject               *object);
 static void              mousepad_replace_dialog_response               (GtkWidget             *widget,
                                                                          gint                   response_id);
@@ -49,6 +48,8 @@ struct _MousepadReplaceDialog
   GtkDialog __parent__;
 
   /* dialog widgets */
+  GtkWidget *search_box;
+  GtkWidget *replace_box;
   GtkWidget *search_entry;
   GtkWidget *replace_entry;
   GtkWidget *find_button;
@@ -90,14 +91,10 @@ static void
 mousepad_replace_dialog_class_init (MousepadReplaceDialogClass *klass)
 {
   GObjectClass   *gobject_class, *entry_class;
-  GtkWidgetClass *gtkwidget_class;
   GtkBindingSet  *binding_set;
 
   gobject_class = G_OBJECT_CLASS (klass);
   gobject_class->finalize = mousepad_replace_dialog_finalize;
-
-  gtkwidget_class = GTK_WIDGET_CLASS (klass);
-  gtkwidget_class->unrealize = mousepad_replace_dialog_unrealize;
 
   dialog_signals[SEARCH] =
     g_signal_new (I_("search"),
@@ -297,7 +294,7 @@ mousepad_replace_dialog_init (MousepadReplaceDialog *dialog)
   gtk_label_set_xalign (GTK_LABEL (label), 0.0);
   gtk_label_set_yalign (GTK_LABEL (label), 0.5);
 
-  combo = gtk_combo_box_text_new_with_entry ();
+  combo = dialog->search_box = gtk_combo_box_text_new_with_entry ();
   mousepad_history_search_fill_search_box (GTK_COMBO_BOX_TEXT (combo));
   gtk_box_pack_start (GTK_BOX (hbox), combo, TRUE, TRUE, 0);
   gtk_label_set_mnemonic_widget (GTK_LABEL (label), combo);
@@ -331,7 +328,7 @@ mousepad_replace_dialog_init (MousepadReplaceDialog *dialog)
   gtk_label_set_xalign (GTK_LABEL (label), 0.0);
   gtk_label_set_yalign (GTK_LABEL (label), 0.5);
 
-  combo = gtk_combo_box_text_new_with_entry ();
+  combo = dialog->replace_box = gtk_combo_box_text_new_with_entry ();
   mousepad_history_search_fill_replace_box (GTK_COMBO_BOX_TEXT (combo));
   gtk_box_pack_start (GTK_BOX (hbox), combo, TRUE, TRUE, 0);
   gtk_label_set_mnemonic_widget (GTK_LABEL (label), combo);
@@ -434,26 +431,6 @@ mousepad_replace_dialog_init (MousepadReplaceDialog *dialog)
 
 
 static void
-mousepad_replace_dialog_unrealize (GtkWidget *widget)
-{
-  MousepadReplaceDialog *dialog = MOUSEPAD_REPLACE_DIALOG (widget);
-  const gchar           *text;
-
-  g_return_if_fail (GTK_IS_ENTRY (dialog->replace_entry));
-  g_return_if_fail (GTK_IS_ENTRY (dialog->search_entry));
-
-  text = gtk_entry_get_text (GTK_ENTRY (dialog->search_entry));
-  mousepad_history_search_insert_search_text (text);
-
-  text = gtk_entry_get_text (GTK_ENTRY (dialog->replace_entry));
-  mousepad_history_search_insert_replace_text (text);
-
-  (*GTK_WIDGET_CLASS (mousepad_replace_dialog_parent_class)->unrealize) (widget);
-}
-
-
-
-static void
 mousepad_replace_dialog_finalize (GObject *object)
 {
   (*G_OBJECT_CLASS (mousepad_replace_dialog_parent_class)->finalize) (object);
@@ -485,8 +462,10 @@ mousepad_replace_dialog_response (GtkWidget *widget,
 {
   MousepadReplaceDialog *dialog = MOUSEPAD_REPLACE_DIALOG (widget);
   MousepadSearchFlags    flags;
+  GtkComboBoxText       *box;
   const gchar           *search_str, *replace_str;
   gint                   search_direction, replace_all_location;
+  guint                  idx;
 
   /* close dialog */
   if (response_id == MOUSEPAD_RESPONSE_CLOSE || response_id < 0)
@@ -494,6 +473,10 @@ mousepad_replace_dialog_response (GtkWidget *widget,
       gtk_widget_destroy (widget);
       return;
     }
+
+  /* get strings */
+  search_str = gtk_entry_get_text (GTK_ENTRY (dialog->search_entry));
+  replace_str = gtk_entry_get_text (GTK_ENTRY (dialog->replace_entry));
 
   /* search direction */
   search_direction = MOUSEPAD_SETTING_GET_UINT (SEARCH_DIRECTION);
@@ -517,6 +500,18 @@ mousepad_replace_dialog_response (GtkWidget *widget,
   /* start position */
   if (response_id == MOUSEPAD_RESPONSE_FIND || response_id == MOUSEPAD_RESPONSE_REVERSE_FIND)
     {
+      /* update search history */
+      box = GTK_COMBO_BOX_TEXT (dialog->search_box);
+      idx = mousepad_history_search_insert_search_text (search_str);
+      if (idx > 0)
+        {
+          gtk_combo_box_text_prepend_text (box, search_str);
+          gtk_combo_box_text_remove (box, idx);
+        }
+
+      /* always be in box: avoid `idx == -1` and `idx == history_size` */
+      gtk_combo_box_set_active (GTK_COMBO_BOX (box), 0);
+
       /* select the first match */
       flags |= MOUSEPAD_SEARCH_FLAGS_ACTION_SELECT;
 
@@ -542,6 +537,28 @@ mousepad_replace_dialog_response (GtkWidget *widget,
     }
   else if (response_id == MOUSEPAD_RESPONSE_REPLACE)
     {
+      /* update search and replace histories: even an implicit search must be added
+       * to the history in this case */
+      box = GTK_COMBO_BOX_TEXT (dialog->search_box);
+      idx = mousepad_history_search_insert_search_text (search_str);
+      if (idx > 0)
+        {
+          gtk_combo_box_text_prepend_text (box, search_str);
+          gtk_combo_box_text_remove (box, idx);
+        }
+
+      gtk_combo_box_set_active (GTK_COMBO_BOX (box), 0);
+
+      box = GTK_COMBO_BOX_TEXT (dialog->replace_box);
+      idx = mousepad_history_search_insert_replace_text (replace_str);
+      if (idx > 0)
+        {
+          gtk_combo_box_text_prepend_text (box, replace_str);
+          gtk_combo_box_text_remove (box, idx);
+        }
+
+      gtk_combo_box_set_active (GTK_COMBO_BOX (box), 0);
+
       /* replace matches */
       flags |= MOUSEPAD_SEARCH_FLAGS_ACTION_REPLACE;
 
@@ -551,10 +568,6 @@ mousepad_replace_dialog_response (GtkWidget *widget,
       else
         flags |= MOUSEPAD_SEARCH_FLAGS_ITER_SEL_START;
     }
-
-  /* get strings */
-  search_str = gtk_entry_get_text (GTK_ENTRY (dialog->search_entry));
-  replace_str = gtk_entry_get_text (GTK_ENTRY (dialog->replace_entry));
 
   /* reset display widgets */
   mousepad_replace_dialog_reset_display (dialog);

mousepad/mousepad-history.c
@@ -992,7 +992,7 @@ mousepad_history_search_fill_replace_box (GtkComboBoxText *box)
 
 
 
-static void
+static guint
 mousepad_history_search_insert_text (const gchar *text,
                                      GHashTable  *history)
 {
@@ -1001,14 +1001,18 @@ mousepad_history_search_insert_text (const gchar *text,
   guint          max_idx, idx;
   gboolean       contains;
 
+  /* history disabled */
+  if (history == NULL)
+    return 0;
+
   /* quit if the search entry is empty */
   if (text == NULL || *text == '\0')
-    return;
+    return 0;
 
   /* quit if the same pattern is searched several times in a row */
   contains = g_hash_table_lookup_extended (history, text, NULL, &value);
   if (contains && (max_idx = GPOINTER_TO_UINT (value)) == 0)
-    return;
+    return 0;
 
   /* update history */
   else
@@ -1025,6 +1029,8 @@ mousepad_history_search_insert_text (const gchar *text,
               else if (idx == max_idx)
                 g_hash_table_iter_replace (&iter, GUINT_TO_POINTER (0));
             }
+
+          max_idx++;
         }
       else
         {
@@ -1038,22 +1044,26 @@ mousepad_history_search_insert_text (const gchar *text,
 
           /* insert new key at first position */
           g_hash_table_insert (history, g_strdup (text), GUINT_TO_POINTER (0));
+
+          max_idx = g_hash_table_size (history);
         }
+
+      return max_idx;
     }
 }
 
 
 
-void
+guint
 mousepad_history_search_insert_search_text (const gchar *text)
 {
-  mousepad_history_search_insert_text (text, search_history);
+  return mousepad_history_search_insert_text (text, search_history);
 }
 
 
 
-void
+guint
 mousepad_history_search_insert_replace_text (const gchar *text)
 {
-  mousepad_history_search_insert_text (text, replace_history);
+  return mousepad_history_search_insert_text (text, replace_history);
 }

mousepad/mousepad-history.h
@@ -71,9 +71,9 @@ void         mousepad_history_search_fill_search_box           (GtkComboBoxText 
 
 void         mousepad_history_search_fill_replace_box          (GtkComboBoxText            *box);
 
-void         mousepad_history_search_insert_search_text        (const gchar                *text);
+guint        mousepad_history_search_insert_search_text        (const gchar                *text);
 
-void         mousepad_history_search_insert_replace_text       (const gchar                *text);
+guint        mousepad_history_search_insert_replace_text       (const gchar                *text);
 
 G_END_DECLS
 

commit 2c7a99693e7e9bd3240adfcb110ffdf442e86d54

search: Do not select first combo box entry when list does not change
Regression introduced in ea239c3f7e3b75cc75470576c6afbcafc157a302 and
19ce10e75b43f406783180f72f0d28a06ac2c470.

This causes a problem when replacing with an empty string, by then
selecting the first entry in the replacement list, thus preventing
replacing again with an empty string. It also selects the first entry in
the search list when searching for an empty string, which is less
annoying but still a bug.

More generally, there is no reason to re-select the first entry if the
list has not changed.

Related: !119

mousepad/mousepad-replace-dialog.c
@@ -507,10 +507,10 @@ mousepad_replace_dialog_response (GtkWidget *widget,
         {
           gtk_combo_box_text_prepend_text (box, search_str);
           gtk_combo_box_text_remove (box, idx);
-        }
 
-      /* always be in box: avoid `idx == -1` and `idx == history_size` */
-      gtk_combo_box_set_active (GTK_COMBO_BOX (box), 0);
+          /* always be in box: avoid `idx == -1` and `idx == history_size` */
+          gtk_combo_box_set_active (GTK_COMBO_BOX (box), 0);
+        }
 
       /* select the first match */
       flags |= MOUSEPAD_SEARCH_FLAGS_ACTION_SELECT;
@@ -545,9 +545,8 @@ mousepad_replace_dialog_response (GtkWidget *widget,
         {
           gtk_combo_box_text_prepend_text (box, search_str);
           gtk_combo_box_text_remove (box, idx);
+          gtk_combo_box_set_active (GTK_COMBO_BOX (box), 0);
         }
-
-      gtk_combo_box_set_active (GTK_COMBO_BOX (box), 0);
 
       box = GTK_COMBO_BOX_TEXT (dialog->replace_box);
       idx = mousepad_history_search_insert_replace_text (replace_str);
@@ -555,9 +554,8 @@ mousepad_replace_dialog_response (GtkWidget *widget,
         {
           gtk_combo_box_text_prepend_text (box, replace_str);
           gtk_combo_box_text_remove (box, idx);
+          gtk_combo_box_set_active (GTK_COMBO_BOX (box), 0);
         }
-
-      gtk_combo_box_set_active (GTK_COMBO_BOX (box), 0);
 
       /* replace matches */
       flags |= MOUSEPAD_SEARCH_FLAGS_ACTION_REPLACE;

mousepad/mousepad-search-bar.c
@@ -410,10 +410,10 @@ mousepad_search_bar_find_string (MousepadSearchBar   *bar,
         {
           gtk_combo_box_text_prepend_text (box, string);
           gtk_combo_box_text_remove (box, idx);
-        }
 
-      /* always be in box: avoid `idx == -1` and `idx == history_size` */
-      gtk_combo_box_set_active (GTK_COMBO_BOX (box), 0);
+          /* always be in box: avoid `idx == -1` and `idx == history_size` */
+          gtk_combo_box_set_active (GTK_COMBO_BOX (box), 0);
+        }
     }
 
   /* reset display widgets */