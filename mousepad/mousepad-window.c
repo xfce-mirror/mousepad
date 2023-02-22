xfce-mirror/mousepad

commit 93980715aff7c02dd5a096e648465265344c19e8

File monitoring: Add an automatic reloading option
Related to !64.

mousepad/mousepad-window.c
@@ -2924,9 +2924,17 @@ mousepad_window_externally_modified (MousepadFile   *file,
                                      MousepadWindow *window)
 {
   MousepadDocument *document = window->active;
+  gboolean modified;
 
   g_return_if_fail (MOUSEPAD_IS_WINDOW (window));
   g_return_if_fail (MOUSEPAD_IS_FILE (file));
+
+  modified = gtk_text_buffer_get_modified (document->buffer);
+  if (! modified && document->file == file && MOUSEPAD_SETTING_GET_BOOLEAN (AUTO_RELOAD))
+    {
+      g_action_group_activate_action (G_ACTION_GROUP (window), "file.reload", NULL);
+      return;
+    }
 
   /* disconnect this handler, the time we ask the user what to do or the file is active */
   mousepad_disconnect_by_func (file, mousepad_window_externally_modified, window);
@@ -2939,8 +2947,7 @@ mousepad_window_externally_modified (MousepadFile   *file,
       g_object_ref (document);
 
       /* ask the user what to do */
-      if (mousepad_dialogs_externally_modified (GTK_WINDOW (window), FALSE,
-                                                gtk_text_buffer_get_modified (document->buffer))
+      if (mousepad_dialogs_externally_modified (GTK_WINDOW (window), FALSE, modified)
           == MOUSEPAD_RESPONSE_RELOAD)
         {
           gtk_text_buffer_set_modified (document->buffer, FALSE);

mousepad/mousepad-application.c
@@ -241,7 +241,8 @@ static const GActionEntry dialog_actions[] =
   /* "File" tab */
   { MOUSEPAD_SETTING_ADD_LAST_EOL, mousepad_application_toggle_activate, NULL, "false", NULL },
   { MOUSEPAD_SETTING_MAKE_BACKUP, mousepad_application_toggle_activate, NULL, "false", NULL },
-  { MOUSEPAD_SETTING_MONITOR_CHANGES, mousepad_application_toggle_activate, NULL, "false", NULL }
+  { MOUSEPAD_SETTING_MONITOR_CHANGES, mousepad_application_toggle_activate, NULL, "false", NULL },
+  { MOUSEPAD_SETTING_AUTO_RELOAD, mousepad_application_toggle_activate, NULL, "false", NULL },
 };
 #define N_DIALOG G_N_ELEMENTS (dialog_actions)
 

mousepad/mousepad-settings.h
@@ -27,6 +27,7 @@ G_BEGIN_DECLS
 #define MOUSEPAD_SETTING_MAKE_BACKUP                  "preferences.file.make-backup"
 #define MOUSEPAD_SETTING_MONITOR_CHANGES              "preferences.file.monitor-changes"
 #define MOUSEPAD_SETTING_MONITOR_DISABLING_TIMER      "preferences.file.monitor-disabling-timer"
+#define MOUSEPAD_SETTING_AUTO_RELOAD                  "preferences.file.auto-reload"
 #define MOUSEPAD_SETTING_SESSION_RESTORE              "preferences.file.session-restore"
 #define MOUSEPAD_SETTING_AUTOSAVE_TIMER               "preferences.file.autosave-timer"
 

mousepad/org.xfce.mousepad.gschema.xml
@@ -107,6 +107,14 @@
         you should leave it alone.
       </description>
     </key>
+    <key name="auto-reload" type="b">
+      <default>false</default>
+      <summary>Automatic reloading</summary>
+      <description>
+        When true Mousepad automatically reloads open and unmodified files in case
+        of external modification.
+      </description>
+    </key>
     <key name="session-restore" enum="org.xfce.mousepad.SessionRestore">
       <default>'after-a-crash'</default>
       <summary>Session restore</summary>

mousepad/resources/ui/mousepad-prefs-dialog.ui
@@ -998,10 +998,48 @@
                 <property name="margin-end">6</property>
                 <property name="spacing">2</property>
                 <child>
-                  <object class="GtkCheckButton">
-                    <property name="label" translatable="yes">File monitoring</property>
+                  <object class="GtkGrid">
                     <property name="visible">True</property>
-                    <property name="action-name">app.preferences.file.monitor-changes</property>
+                    <property name="margin-top">0</property>
+                    <property name="margin-bottom">0</property>
+                    <property name="margin-start">0</property>
+                    <property name="margin-end">0</property>
+                    <property name="column-spacing">4</property>
+                    <property name="row-spacing">4</property>
+                    <child>
+                      <object class="GtkCheckButton" id="/prefs/file/miscellaneous/monitor-changes">
+                        <property name="visible">True</property>
+                        <property name="action-name">app.preferences.file.monitor-changes</property>
+                      </object>
+                      <packing>
+                        <property name="left-attach">0</property>
+                        <property name="top-attach">0</property>
+                      </packing>
+                    </child>
+                    <child>
+                      <object class="GtkLabel">
+                        <property name="visible">True</property>
+                        <property name="xalign">0</property>
+                        <property name="label" translatable="yes">File monitoring</property>
+                      </object>
+                      <packing>
+                        <property name="left-attach">1</property>
+                        <property name="top-attach">0</property>
+                      </packing>
+                    </child>
+                    <child>
+                      <object class="GtkCheckButton">
+                        <property name="label" translatable="yes">Automatic reloading</property>
+                        <property name="visible">True</property>
+                        <property name="action-name">app.preferences.file.auto-reload</property>
+                        <property name="sensitive" bind-source="/prefs/file/miscellaneous/monitor-changes"
+                                  bind-property="active" bind-flags="G_BINDING_SYNC_CREATE"/>
+                      </object>
+                      <packing>
+                        <property name="left-attach">1</property>
+                        <property name="top-attach">1</property>
+                      </packing>
+                    </child>
                   </object>
                 </child>
                 <child>

commit 7ea1abdad87bcb2badd238cc881356832d541541

Properly reconnect signal handler when auto-reload is enabled
It was disconnected when a file in an inactive tab was externally
modified, and never reconnected when the file became active, so file
monitoring was broken for this file from there.

Fixes: 93980715aff7c02dd5a096e648465265344c19e8

mousepad/mousepad-window.c
@@ -3027,15 +3027,18 @@ mousepad_window_externally_modified (MousepadFile   *file,
   g_return_if_fail (MOUSEPAD_IS_WINDOW (window));
   g_return_if_fail (MOUSEPAD_IS_FILE (file));
 
+  /* disconnect this handler, the time we ask the user what to do or the file is loadable */
+  mousepad_disconnect_by_func (file, mousepad_window_externally_modified, window);
+
+  /* auto-reload the file if it's unmodified and in the active tab (active window or not) */
   modified = gtk_text_buffer_get_modified (document->buffer);
   if (! modified && document->file == file && MOUSEPAD_SETTING_GET_BOOLEAN (AUTO_RELOAD))
     {
+      g_signal_connect (file, "externally-modified",
+                        G_CALLBACK (mousepad_window_externally_modified), window);
       g_action_group_activate_action (G_ACTION_GROUP (window), "file.reload", NULL);
       return;
     }
-
-  /* disconnect this handler, the time we ask the user what to do or the file is active */
-  mousepad_disconnect_by_func (file, mousepad_window_externally_modified, window);
 
   /* the file is active */
   if (document->file == file && gtk_window_is_active (GTK_WINDOW (window)))