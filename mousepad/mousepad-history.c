xfce-mirror/mousepad

commit af9572ea8a7b66c79db2c86b3e002d0d4218b96b

Add session backup and restore
The list of tabs per window is saved at different times, in particular
when switching tab or active window. This way, an accurate picture of
the current session is kept at all times, in a format described in the
`.gschema.xml` file.

When activating "app.quit" or when closing the last window, session
backup is suspended to keep the current state as the one to be restored
on next session.

In general, session restore takes precedence over any command line
argument. In particular, a restored file is opened with encoding and
cursor position found in the recent history (or default values), even
if it is specified on the command line with or without any option.

Files specified on the command line that are not included in the list
of files to restore are opened normally, from the state in which the
application is at the end of the session restore, just as if they had
been opened while the application was already running.

Closes #21, see !108 for more details.

mousepad/mousepad-history.c
@@ -19,6 +19,8 @@
 #include <mousepad/mousepad-settings.h>
 #include <mousepad/mousepad-dialogs.h>
 #include <mousepad/mousepad-util.h>
+#include <mousepad/mousepad-document.h>
+#include <mousepad/mousepad-window.h>
 
 
 
@@ -269,4 +271,170 @@ mousepad_history_recent_clear (void)
       mousepad_dialogs_show_error (window, error, _("Failed to clear the recent history"));
       g_error_free (error);
     }
+}
+
+
+
+void
+mousepad_history_session_save (gboolean toggle)
+{
+  MousepadDocument  *document;
+  GtkNotebook       *notebook;
+  GList             *list, *li;
+  gchar            **session;
+  gchar             *uri;
+  const gchar       *fmt;
+  guint              length = 0, id, current, n_pages, n;
+
+  static gboolean blocked = FALSE;
+
+  /* toggle blocked state if needed */
+  if (toggle)
+    blocked = ! blocked;
+
+  /* exit if session remembering is blocked or disabled */
+  if (blocked || ! MOUSEPAD_SETTING_GET_BOOLEAN (REMEMBER_SESSION))
+    return;
+
+  /* compute the maximum length of the session array */
+  list = gtk_application_get_windows (GTK_APPLICATION (g_application_get_default ()));
+  for (li = list; li != NULL; li = li->next)
+    {
+      notebook = GTK_NOTEBOOK (mousepad_window_get_notebook (li->data));
+      length += gtk_notebook_get_n_pages (notebook);
+    }
+
+  /* allocate the session array */
+  session = g_malloc0 ((length + 1) * sizeof (gchar *));
+
+  /* fill in the session array */
+  for (li = list, length = 0; li != NULL; li = li->next)
+  {
+    id = gtk_application_window_get_id (li->data);
+    notebook = GTK_NOTEBOOK (mousepad_window_get_notebook (li->data));
+    current = gtk_notebook_get_current_page (notebook);
+    n_pages = gtk_notebook_get_n_pages (notebook);
+    for (n = 0; n < n_pages; n++)
+      {
+        document = MOUSEPAD_DOCUMENT (gtk_notebook_get_nth_page (notebook, n));
+        if (mousepad_file_location_is_set (document->file))
+          {
+            fmt = (n == current ? "%d::%s" : "%d:%s");
+            uri = mousepad_file_get_uri (document->file);
+            session[length++] = g_strdup_printf (fmt, id, uri);
+            g_free (uri);
+          }
+      }
+  }
+
+  /* save the session array */
+  MOUSEPAD_SETTING_SET_STRV (SESSION, (const gchar *const *) session);
+
+  /* cleanup */
+  g_strfreev (session);
+}
+
+
+
+static void
+mousepad_history_remember_session_changed (void)
+{
+  /* first session save */
+  if (MOUSEPAD_SETTING_GET_BOOLEAN (REMEMBER_SESSION))
+    mousepad_history_session_save (FALSE);
+  /* clear session array */
+  else
+    MOUSEPAD_SETTING_RESET (SESSION);
+}
+
+
+
+gboolean
+mousepad_history_session_restore (MousepadApplication *application)
+{
+  GtkWindow    *window;
+  GtkWidget    *notebook;
+  GFile       **files;
+  GFile        *file;
+  gchar       **session, **p;
+  const gchar  *uri;
+  guint         n_uris, n_files, n, sid, wid, current;
+
+  /* initialize session management */
+  MOUSEPAD_SETTING_CONNECT (REMEMBER_SESSION,
+                            G_CALLBACK (mousepad_history_remember_session_changed), NULL, 0);
+  if (! MOUSEPAD_SETTING_GET_BOOLEAN (REMEMBER_SESSION))
+    {
+      MOUSEPAD_SETTING_RESET (SESSION);
+      return FALSE;
+    }
+
+  /* get the session array */
+  session = MOUSEPAD_SETTING_GET_STRV (SESSION);
+  n_uris = g_strv_length (session);
+  if (n_uris == 0)
+   {
+     g_strfreev (session);
+     return FALSE;
+   }
+
+  /* walk the session array in reverse order: last open, first display plan */
+  sid = g_signal_lookup ("open", G_TYPE_APPLICATION);
+  p = session + n_uris;
+  do
+    {
+      /* get the number of files for this window */
+      wid = atoi (*(--p));
+      n_files = 1;
+      while (p != session && atoi (*(--p)) == (gint) wid)
+        n_files++;
+
+      /* readjust position */
+      if (p != session)
+        p++;
+
+      /* allocate the GFile array */
+      files = g_malloc (n_files * sizeof (GFile *));
+
+      /* add files from the session array */
+      for (n = 0, current = 0; n < n_files; n++)
+        {
+          /* get the uri, removing the window id and eventually the current tab mark */
+          uri = g_strstr_len (*(p + n), -1, ":") + 1;
+          if (*uri == ':')
+            {
+              current = n;
+              uri++;
+            }
+
+          file = g_file_new_for_uri (uri);
+          if (g_file_query_exists (file, NULL))
+            files[n] = file;
+          else
+            {
+              n_files--;
+              g_object_unref (file);
+            }
+        }
+
+      /* open files */
+      g_signal_emit (application, sid, 0, files, n_files, NULL, NULL);
+
+      /* set current tab */
+      window = gtk_application_get_active_window (GTK_APPLICATION (application));
+      notebook = mousepad_window_get_notebook (MOUSEPAD_WINDOW (window));
+      gtk_notebook_set_current_page (GTK_NOTEBOOK (notebook), current);
+
+      /* free the GFile array */
+      for (n = 0; n < n_files; n++)
+        g_object_unref (files[n]);
+
+      g_free (files);
+    }
+  while (p != session);
+
+  /* cleanup */
+  g_strfreev (session);
+
+  return TRUE;
 }

mousepad/mousepad-application.c
@@ -932,9 +932,9 @@ mousepad_application_command_line (GApplication            *gapplication,
   GFile               **files;
   GFile                *file;
   const gchar          *opening_mode;
-  gchar               **filenames = NULL;
+  const gchar         **filenames = NULL;
   gint                  n, n_files;
-  gboolean              user_set_encoding, user_set_cursor = FALSE;
+  gboolean              user_set_encoding, user_set_cursor = FALSE, restored = FALSE;
 
   /* get the option dictionary */
   options = g_application_command_line_get_options_dict (command_line);
@@ -953,6 +953,14 @@ mousepad_application_command_line (GApplication            *gapplication,
         }
 
       return EXIT_SUCCESS;
+    }
+
+  /* restore previous session */
+  if (! g_application_command_line_get_is_remote (command_line))
+    {
+      application->opening_mode = MIXED;
+      application->encoding = mousepad_encoding_get_default ();
+      restored = mousepad_history_session_restore (application);
     }
 
   /* retrieve encoding from the remote instance */
@@ -1001,8 +1009,8 @@ mousepad_application_command_line (GApplication            *gapplication,
   /* extract filenames */
   g_variant_dict_lookup (options, G_OPTION_REMAINING, "^a&ay", &filenames);
 
-  /* open files provided on the command line or an empty document */
-  if (filenames != NULL && (n_files = g_strv_length (filenames)) > 0)
+  /* open files provided on the command line */
+  if (filenames != NULL && (n_files = g_strv_length ((gchar **) filenames)) > 0)
     {
       /* prepare the GFile array */
       files = g_malloc (n_files * sizeof (GFile *));
@@ -1025,7 +1033,8 @@ mousepad_application_command_line (GApplication            *gapplication,
 
       g_free (files);
     }
-  else
+  /* open an empty document if previous session wasn't restored */
+  else if (! restored)
     g_application_activate (gapplication);
 
   /* cleanup */
@@ -1210,6 +1219,9 @@ mousepad_application_create_window (MousepadApplication *application)
                     G_CALLBACK (mousepad_application_new_window_with_document), application);
   g_signal_connect (window, "new-window",
                     G_CALLBACK (mousepad_application_new_window), application);
+  g_signal_connect_object (mousepad_window_get_notebook (MOUSEPAD_WINDOW (window)), "switch-page",
+                           G_CALLBACK (mousepad_history_session_save), NULL,
+                           G_CONNECT_SWAPPED | G_CONNECT_AFTER);
 
   return window;
 }
@@ -1283,6 +1295,9 @@ mousepad_application_active_window_changed (MousepadApplication *application)
 
       /* update window dependent menu items */
       mousepad_window_update_window_menu_items (app_windows->data);
+
+      /* save new session state */
+      mousepad_history_session_save (FALSE);
     }
 
   /* store a copy of the application windows list to compare at next call */
@@ -1593,6 +1608,9 @@ mousepad_application_action_quit (GSimpleAction *action,
   GList   *windows, *window;
   GAction *close;
 
+  /* block session handler */
+  mousepad_history_session_save (TRUE);
+
   /* try to close all windows, abort at the first failure */
   windows = g_list_copy (gtk_application_get_windows (data));
   for (window = windows; window != NULL; window = window->next)
@@ -1600,7 +1618,12 @@ mousepad_application_action_quit (GSimpleAction *action,
       close = g_action_map_lookup_action (G_ACTION_MAP (window->data), "file.close-window");
       g_action_activate (close, NULL);
       if (! mousepad_action_get_state_int32_boolean (close))
-        break;
+        {
+          /* unblock session handler and save session */
+          mousepad_history_session_save (TRUE);
+
+          break;
+        }
     }
 
   g_list_free (windows);

mousepad/mousepad-history.h
@@ -19,6 +19,7 @@
 
 #include <mousepad/mousepad-encoding.h>
 #include <mousepad/mousepad-file.h>
+#include <mousepad/mousepad-application.h>
 
 G_BEGIN_DECLS
 
@@ -37,6 +38,10 @@ void         mousepad_history_recent_get_cursor                (GFile           
                                                                 gint                       *column);
 
 void         mousepad_history_recent_clear                     (void);
+
+void         mousepad_history_session_save                     (gboolean                    toggle);
+
+gboolean     mousepad_history_session_restore                  (MousepadApplication        *application);
 
 G_END_DECLS
 

mousepad/mousepad-settings.c
@@ -169,6 +169,22 @@ mousepad_setting_disconnect (const gchar *setting,
 
 
 void
+mousepad_setting_reset (const gchar *setting)
+{
+  const gchar *key_name;
+  GSettings   *settings;
+
+  g_return_if_fail (setting != NULL);
+
+  if (mousepad_settings_store_lookup (settings_store, setting, &key_name, &settings))
+    g_settings_reset (settings, key_name);
+  else
+    g_warn_if_reached ();
+}
+
+
+
+void
 mousepad_setting_get (const gchar *setting,
                       const gchar *format_string,
                       ...)

mousepad/mousepad-settings.h
@@ -27,6 +27,8 @@ G_BEGIN_DECLS
 #define MOUSEPAD_SETTING_MAKE_BACKUP                  "preferences.file.make-backup"
 #define MOUSEPAD_SETTING_MONITOR_CHANGES              "preferences.file.monitor-changes"
 #define MOUSEPAD_SETTING_MONITOR_DISABLING_TIMER      "preferences.file.monitor-disabling-timer"
+#define MOUSEPAD_SETTING_REMEMBER_SESSION             "preferences.file.remember-session"
+
 #define MOUSEPAD_SETTING_AUTO_INDENT                  "preferences.view.auto-indent"
 #define MOUSEPAD_SETTING_FONT                         "preferences.view.font-name"
 #define MOUSEPAD_SETTING_USE_DEFAULT_FONT             "preferences.view.use-default-monospace-font"
@@ -49,6 +51,7 @@ G_BEGIN_DECLS
 #define MOUSEPAD_SETTING_WORD_WRAP                    "preferences.view.word-wrap"
 #define MOUSEPAD_SETTING_MATCH_BRACES                 "preferences.view.match-braces"
 #define MOUSEPAD_SETTING_COLOR_SCHEME                 "preferences.view.color-scheme"
+
 #define MOUSEPAD_SETTING_TOOLBAR_STYLE                "preferences.window.toolbar-style"
 #define MOUSEPAD_SETTING_TOOLBAR_ICON_SIZE            "preferences.window.toolbar-icon-size"
 #define MOUSEPAD_SETTING_ALWAYS_SHOW_TABS             "preferences.window.always-show-tabs"
@@ -71,6 +74,8 @@ G_BEGIN_DECLS
 
 /* State setting names */
 #define MOUSEPAD_SETTING_ENABLED_PLUGINS              "state.application.enabled-plugins"
+#define MOUSEPAD_SETTING_SESSION                      "state.application.session"
+
 #define MOUSEPAD_SETTING_SEARCH_DIRECTION             "state.search.direction"
 #define MOUSEPAD_SETTING_SEARCH_WRAP_AROUND           "state.search.wrap-around"
 #define MOUSEPAD_SETTING_SEARCH_MATCH_CASE            "state.search.match-case"
@@ -80,6 +85,7 @@ G_BEGIN_DECLS
 #define MOUSEPAD_SETTING_SEARCH_REPLACE_ALL_LOCATION  "state.search.replace-all-location"
 #define MOUSEPAD_SETTING_SEARCH_HIGHLIGHT_ALL         "state.search.highlight-all"
 #define MOUSEPAD_SETTING_SEARCH_INCREMENTAL           "state.search.incremental"
+
 #define MOUSEPAD_SETTING_WINDOW_HEIGHT                "state.window.height"
 #define MOUSEPAD_SETTING_WINDOW_WIDTH                 "state.window.width"
 #define MOUSEPAD_SETTING_WINDOW_TOP                   "state.window.top"
@@ -111,6 +117,8 @@ void       mousepad_setting_disconnect     (const gchar        *setting,
                                             gpointer            user_data);
 
 /* functions for reading and writing settings */
+
+void       mousepad_setting_reset          (const gchar        *setting);
 
 void       mousepad_setting_get            (const gchar        *setting,
                                             const gchar        *format_string,
@@ -170,6 +178,8 @@ void       mousepad_setting_set_variant    (const gchar        *setting,
 
 #define MOUSEPAD_SETTING_DISCONNECT(setting, callback, user_data) \
   mousepad_setting_disconnect (MOUSEPAD_SETTING_##setting, callback, user_data)
+
+#define MOUSEPAD_SETTING_RESET(setting) mousepad_setting_reset (MOUSEPAD_SETTING_##setting)
 
 #define MOUSEPAD_SETTING_GET(setting, ...)           mousepad_setting_get (MOUSEPAD_SETTING_##setting, __VA_ARGS__)
 #define MOUSEPAD_SETTING_GET_BOOLEAN(setting)        mousepad_setting_get_boolean (MOUSEPAD_SETTING_##setting)

mousepad/mousepad-window.c
@@ -4884,9 +4884,13 @@ mousepad_window_action_close_window (GSimpleAction *action,
   if ((npages = gtk_notebook_get_n_pages (GTK_NOTEBOOK (window->notebook))) == 0)
     {
       gtk_widget_destroy (GTK_WIDGET (window));
-
       return;
     }
+
+  /* disconnect session handler from this notebook: if it is the last window, the session
+   * should not be saved anymore, else it will be saved when the active window changes */
+  mousepad_disconnect_by_func (window->notebook,
+                               G_CALLBACK (mousepad_history_session_save), NULL);
 
   /* prevent menu updates */
   lock_menu_updates++;
@@ -4911,6 +4915,12 @@ mousepad_window_action_close_window (GSimpleAction *action,
 
           /* store the close result as the action state */
           g_action_change_state (G_ACTION (action), g_variant_new_int32 (FALSE));
+
+          /* save session and reconnect handler */
+          mousepad_history_session_save (FALSE);
+          g_signal_connect_object (window->notebook, "switch-page",
+                                   G_CALLBACK (mousepad_history_session_save), NULL,
+                                   G_CONNECT_SWAPPED | G_CONNECT_AFTER);
 
           /* leave function */
           return;

mousepad/org.xfce.mousepad.gschema.xml
@@ -99,6 +99,15 @@
         you should leave it alone.
       </description>
     </key>
+    <key name="remember-session" type="b">
+      <default>false</default>
+      <summary>Remember session</summary>
+      <description>
+        When true Mousepad regularly saves the session state, i.e a list of open
+        tabs and windows (see the 'session' key in the 'state.application' schema),
+        and restore this state on next session.
+      </description>
+    </key>
   </schema>
 
   <!-- Textview preferences -->
@@ -446,6 +455,15 @@
       <summary>Enabled plugins</summary>
       <description>
         List of enabled plugins, among those found in the plugin directory.
+      </description>
+    </key>
+    <key name="session" type="as">
+      <default>[]</default>
+      <summary>Session</summary>
+      <description>
+        List of tabs per window for a session, in the form 'n:[:]uri' where 'n' is an
+        integer uniquely identifying a window, 'uri' the file uri for a tab, and the
+        optional ':' identifies the active tab in the window (the last one wins).
       </description>
     </key>
   </schema>

commit 0ea1ca1d9318292c28183c30843dec59423238a3

history: Properly restore multi-window session
A bug that has existed since the feature was implemented. When several
windows are open and the active window at exit time contains only one
tab, this window is not correctly restored. The tab it contains
overwrites the last tab of the previous window instead.

Fixes: af9572ea8a7b66c79db2c86b3e002d0d4218b96b
Related: !108

mousepad/mousepad-history.c
@@ -580,7 +580,7 @@ mousepad_history_session_restore (MousepadApplication *application)
         n_uris++;
 
       /* readjust position */
-      if (p != session)
+      if (atoi (*p) != (gint) wid)
         p++;
 
       /* allocate the GFile array */