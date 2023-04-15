xfce-mirror/mousepad

commit 2c37581b7b062c09d687ca825b839ee25ed86bd5

Apply opening mode also to the open dialog (Fixes #181)

mousepad/mousepad-application.c
@@ -711,6 +711,14 @@ mousepad_application_set_accels (MousepadApplication *application)
 
 
 
+static void
+mousepad_application_opening_mode_changed (MousepadApplication *application)
+{
+  application->opening_mode = MOUSEPAD_SETTING_GET_ENUM (OPENING_MODE);
+}
+
+
+
 static gint
 mousepad_application_sort_plugins (gconstpointer a,
                                    gconstpointer b)
@@ -867,6 +875,10 @@ mousepad_application_startup (GApplication *gapplication)
 
       g_settings_schema_unref (schema);
     }
+
+  /* keep opening mode in sync for the open dialog */
+  MOUSEPAD_SETTING_CONNECT_OBJECT (OPENING_MODE, mousepad_application_opening_mode_changed,
+                                   application, G_CONNECT_SWAPPED);
 
   /* add application actions */
   g_action_map_add_action_entries (G_ACTION_MAP (application), stateless_actions,
@@ -1138,6 +1150,9 @@ mousepad_application_open (GApplication  *gapplication,
             gtk_widget_destroy (window);
         }
     }
+
+  /* restore opening mode from the settings if it was overridden from the command line */
+  application->opening_mode = MOUSEPAD_SETTING_GET_ENUM (OPENING_MODE);
 }
 
 

mousepad/mousepad-window.c
@@ -4545,6 +4545,8 @@ mousepad_window_action_open (GSimpleAction *action,
   MousepadWindow   *window = data;
   MousepadEncoding  encoding;
   GSList           *files, *file;
+  GFile           **files_array;
+  gint              n, n_files;
 
   g_return_if_fail (MOUSEPAD_IS_WINDOW (window));
   g_return_if_fail (MOUSEPAD_IS_DOCUMENT (window->active));
@@ -4558,11 +4560,17 @@ mousepad_window_action_open (GSimpleAction *action,
       /* lock menu updates */
       lock_menu_updates++;
 
-      /* open all the selected locations in new tabs */
-      for (file = files; file != NULL; file = file->next)
-        mousepad_window_open_file (window, file->data, encoding, 0, 0, TRUE);
+      /* prepare GFile array */
+      n_files = g_slist_length (files);
+      files_array = g_new (GFile *, n_files);
+      for (n = 0, file = files; file != NULL; n++, file = file->next)
+        files_array[n] = file->data;
+
+      /* open selected locations according to the application's opening mode */
+      g_signal_emit_by_name (g_application_get_default (), "open", files_array, n_files, NULL);
 
       /* cleanup */
+      g_free (files_array);
       g_slist_free_full (files, g_object_unref);
 
       /* allow menu updates again */

commit fc0ce1c186994f160472daf9114fc56481bf614e

Restore opening mode only when it was overridden from the command line
This caused at least a regression when restoring the previous session,
where the opening mode must remain at "mixed" throughout the
restoration.

Fixes: 2c37581b7b062c09d687ca825b839ee25ed86bd5

mousepad/mousepad-application.c
@@ -1057,6 +1057,9 @@ mousepad_application_command_line (GApplication            *gapplication,
   else if (! restored)
     g_application_activate (gapplication);
 
+  /* restore opening mode from the settings if it was overridden from the command line */
+  application->opening_mode = MOUSEPAD_SETTING_GET_ENUM (OPENING_MODE);
+
   /* cleanup */
   g_free (filenames);
 
@@ -1150,9 +1153,6 @@ mousepad_application_open (GApplication  *gapplication,
             gtk_widget_destroy (window);
         }
     }
-
-  /* restore opening mode from the settings if it was overridden from the command line */
-  application->opening_mode = MOUSEPAD_SETTING_GET_ENUM (OPENING_MODE);
 }
 
 