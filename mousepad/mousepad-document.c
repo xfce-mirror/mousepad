xfce-mirror/mousepad

commit 9dae3d1457ab64a05ae586cac985479c7398e295

Add "X of Y matches" to search bar and replace dialog (Closes #174)

mousepad/mousepad-document.c
@@ -86,6 +86,7 @@ struct _MousepadDocumentPrivate
   GtkSourceBuffer        *selection_buffer;
   gint                    prev_search_state;
   guint                   search_id;
+  gint                    cur_match;
 };
 
 
@@ -138,8 +139,8 @@ mousepad_document_class_init (MousepadDocumentClass *klass)
 
   document_signals[SEARCH_COMPLETED] =
     g_signal_new (I_("search-completed"), G_TYPE_FROM_CLASS (gobject_class), G_SIGNAL_RUN_LAST,
-                  0, NULL, NULL, _mousepad_marshal_VOID__INT_STRING_FLAGS,
-                  G_TYPE_NONE, 3, G_TYPE_INT, G_TYPE_STRING, MOUSEPAD_TYPE_SEARCH_FLAGS);
+                  0, NULL, NULL, _mousepad_marshal_VOID__INT_INT_STRING_FLAGS,
+                  G_TYPE_NONE, 4, G_TYPE_INT, G_TYPE_INT, G_TYPE_STRING, MOUSEPAD_TYPE_SEARCH_FLAGS);
 }
 
 
@@ -226,6 +227,7 @@ mousepad_document_init (MousepadDocument *document)
   document->priv->search_context = gtk_source_search_context_new (
                                      GTK_SOURCE_BUFFER (document->buffer), NULL);
   document->priv->search_id = 0;
+  document->priv->cur_match = 0;
 
   /* bind search settings to Mousepad settings, except "regex-enabled" to prevent prohibitive
    * computation times in some situations (see
@@ -323,6 +325,13 @@ mousepad_document_notify_cursor_position (MousepadDocument *document)
 
   /* get length of the selection */
   selection = mousepad_view_get_selection_length (document->textview);
+
+  /* clear search index if set */
+  if (document->priv->cur_match != 0)
+    {
+      document->priv->cur_match = 0;
+      g_object_notify (G_OBJECT (document->priv->search_context), "occurrences-count");
+    }
 
   /* emit the signal */
   g_signal_emit (document, document_signals[CURSOR_CHANGED], 0, line, column, selection);
@@ -624,7 +633,11 @@ mousepad_document_search_completed_idle (gpointer data)
   /* handle the action */
   if (found && (flags & MOUSEPAD_SEARCH_FLAGS_ACTION_SELECT)
       && ! (flags & MOUSEPAD_SEARCH_FLAGS_AREA_SELECTION))
-    gtk_text_buffer_select_range (document->buffer, start, end);
+    {
+      gtk_text_buffer_select_range (document->buffer, start, end);
+      document->priv->cur_match =
+        gtk_source_search_context_get_occurrence_position (search_context, start, end);
+    }
   else if (flags & MOUSEPAD_SEARCH_FLAGS_ACTION_REPLACE)
     {
       if (found && ! (flags & MOUSEPAD_SEARCH_FLAGS_ENTIRE_AREA))
@@ -667,6 +680,12 @@ mousepad_document_search_completed_idle (gpointer data)
   else if (! (flags & MOUSEPAD_SEARCH_FLAGS_ACTION_NONE)
            && ! (flags & MOUSEPAD_SEARCH_FLAGS_AREA_SELECTION))
     gtk_text_buffer_place_cursor (document->buffer, &iter);
+
+  /* force the signal emission, to cover cases where Mousepad search settings change without
+   * changing GtkSourceView settings (e.g. when switching between single-document mode and
+   * multi-document mode, or if search index changed) */
+  if (gtk_source_search_context_get_occurrences_count (search_context) != -1)
+    g_object_notify (G_OBJECT (search_context), "occurrences-count");
 
   document->priv->search_id = 0;
 
@@ -738,12 +757,6 @@ mousepad_document_search_completed (GObject      *object,
       g_warning ("%s", error->message);
       g_error_free (error);
     }
-
-  /* force the signal emission, to cover cases where Mousepad search settings change without
-   * changing GtkSourceView settings (e.g. when switching between single-document mode and
-   * multi-document mode) */
-  if (gtk_source_search_context_get_occurrences_count (search_context) != -1)
-    g_object_notify (G_OBJECT (search_context), "occurrences-count");
 
   /* now we need first to exit this async task callback to prevent any warning,
    * but we launch the rest as soon as possible to preserve real-time behavior */
@@ -890,7 +903,8 @@ mousepad_document_emit_search_signal (MousepadDocument       *document,
   string = gtk_source_search_settings_get_search_text (search_settings);
 
   /* emit the signal */
-  g_signal_emit (document, document_signals[SEARCH_COMPLETED], 0, n_matches, string, flags);
+  g_signal_emit (document, document_signals[SEARCH_COMPLETED], 0, document->priv->cur_match,
+                 n_matches, string, flags);
 }
 
 

mousepad/mousepad-marshal.list
@@ -1,4 +1,4 @@
 VOID:INT,INT,INT
-VOID:INT,STRING,FLAGS
+VOID:INT,INT,STRING,FLAGS
 VOID:FLAGS,STRING,STRING
 VOID:OBJECT,INT,INT

mousepad/mousepad-replace-dialog.c
@@ -28,6 +28,7 @@ static void              mousepad_replace_dialog_finalize               (GObject
 static void              mousepad_replace_dialog_response               (GtkWidget             *widget,
                                                                          gint                   response_id);
 static void              mousepad_replace_dialog_search_completed       (MousepadReplaceDialog *dialog,
+                                                                         gint                   cur_match,
                                                                          gint                   n_matches,
                                                                          const gchar           *search_string,
                                                                          MousepadSearchFlags    flags);
@@ -225,7 +226,7 @@ mousepad_replace_dialog_post_init (MousepadReplaceDialog *dialog)
   /* give the dialog its definite size by setting a fake occurrences label */
   gtk_entry_grab_focus_without_selecting (GTK_ENTRY (dialog->search_entry));
   gtk_entry_set_text (GTK_ENTRY (dialog->search_entry), "fake-text");
-  mousepad_replace_dialog_search_completed (dialog, 99999, "fake-text",
+  mousepad_replace_dialog_search_completed (dialog, 99999, 99999, "fake-text",
                                             MOUSEPAD_SEARCH_FLAGS_AREA_SELECTION
                                             | MOUSEPAD_SEARCH_FLAGS_AREA_ALL_DOCUMENTS);
 
@@ -573,6 +574,7 @@ mousepad_replace_dialog_response (GtkWidget *widget,
 
 static void
 mousepad_replace_dialog_search_completed (MousepadReplaceDialog *dialog,
+                                          gint                   cur_match,
                                           gint                   n_matches,
                                           const gchar           *search_string,
                                           MousepadSearchFlags    flags)
@@ -608,8 +610,12 @@ mousepad_replace_dialog_search_completed (MousepadReplaceDialog *dialog,
       mousepad_util_entry_error (dialog->search_entry, n_matches == 0);
 
       /* update counter */
-      message = g_strdup_printf (ngettext ("%d occurrence", "%d occurrences", n_matches),
-                                 n_matches);
+      if (cur_match != 0)
+        message = g_strdup_printf (ngettext ("%d of %d match", "%d of %d matches", n_matches),
+                                   cur_match, n_matches);
+      else
+        message = g_strdup_printf (ngettext ("%d match", "%d matches", n_matches),
+                                   n_matches);
       gtk_label_set_markup (GTK_LABEL (dialog->hits_label), message);
       g_free (message);
     }

mousepad/mousepad-search-bar.c
@@ -28,6 +28,7 @@ static void      mousepad_search_bar_finalize                   (GObject        
 static void      mousepad_search_bar_find_string                (MousepadSearchBar       *bar,
                                                                  MousepadSearchFlags      flags);
 static void      mousepad_search_bar_search_completed           (MousepadSearchBar       *bar,
+                                                                 gint                     cur_match,
                                                                  gint                     n_matches,
                                                                  const gchar             *search_string,
                                                                  MousepadSearchFlags      flags);
@@ -436,6 +437,7 @@ mousepad_search_bar_find_string (MousepadSearchBar   *bar,
 
 static void
 mousepad_search_bar_search_completed (MousepadSearchBar   *bar,
+                                      gint                 cur_match,
                                       gint                 n_matches,
                                       const gchar         *search_string,
                                       MousepadSearchFlags  flags)
@@ -462,8 +464,12 @@ mousepad_search_bar_search_completed (MousepadSearchBar   *bar,
       mousepad_util_entry_error (bar->entry, n_matches == 0);
 
       /* update counter */
-      message = g_strdup_printf (ngettext ("%d occurrence", "%d occurrences", n_matches),
-                                 n_matches);
+      if (cur_match != 0)
+        message = g_strdup_printf (ngettext ("%d of %d match", "%d of %d matches", n_matches),
+                                   cur_match, n_matches);
+      else
+        message = g_strdup_printf (ngettext ("%d match", "%d matches", n_matches),
+                                   n_matches);
       gtk_label_set_markup (GTK_LABEL (bar->hits_label), message);
       g_free (message);
     }

mousepad/mousepad-window.c
@@ -218,6 +218,7 @@ static void              mousepad_window_search                       (MousepadW
                                                                        const gchar            *string,
                                                                        const gchar            *replacement);
 static void              mousepad_window_search_completed             (MousepadDocument       *document,
+                                                                       gint                    cur_match_doc,
                                                                        gint                    n_matches_doc,
                                                                        const gchar            *string,
                                                                        MousepadSearchFlags     flags,
@@ -633,8 +634,8 @@ mousepad_window_class_init (MousepadWindowClass *klass)
                   G_TYPE_FROM_CLASS (gobject_class),
                   G_SIGNAL_RUN_LAST,
                   0, NULL, NULL,
-                  _mousepad_marshal_VOID__INT_STRING_FLAGS,
-                  G_TYPE_NONE, 3, G_TYPE_INT, G_TYPE_STRING,
+                  _mousepad_marshal_VOID__INT_INT_STRING_FLAGS,
+                  G_TYPE_NONE, 4, G_TYPE_INT, G_TYPE_INT, G_TYPE_STRING,
                   MOUSEPAD_TYPE_SEARCH_FLAGS);
 
   g_object_class_install_property (gobject_class, PROP_SEARCH_WIDGET_VISIBLE,
@@ -4082,6 +4083,7 @@ mousepad_window_search (MousepadWindow      *window,
 
 static void
 mousepad_window_search_completed (MousepadDocument    *document,
+                                  gint                 cur_match_doc,
                                   gint                 n_matches_doc,
                                   const gchar         *string,
                                   MousepadSearchFlags  flags,
@@ -4096,8 +4098,8 @@ mousepad_window_search_completed (MousepadDocument    *document,
   /* always send the active document result, although it will only be relevant for the
    * search bar if the multi-document mode is active */
   if (document == window->active)
-    g_signal_emit (window, window_signals[SEARCH_COMPLETED], 0, n_matches_doc, string,
-                   flags & (~ MOUSEPAD_SEARCH_FLAGS_AREA_ALL_DOCUMENTS));
+    g_signal_emit (window, window_signals[SEARCH_COMPLETED], 0, cur_match_doc, n_matches_doc,
+                   string, flags & (~ MOUSEPAD_SEARCH_FLAGS_AREA_ALL_DOCUMENTS));
 
   /* multi-document mode is active: collect the search results regardless of their origin:
    * replace dialog, search bar or buffer change */
@@ -4178,7 +4180,7 @@ mousepad_window_search_completed (MousepadDocument    *document,
         return;
 
       /* send the final result, only relevant for the replace dialog */
-      g_signal_emit (window, window_signals[SEARCH_COMPLETED], 0, n_matches, string,
+      g_signal_emit (window, window_signals[SEARCH_COMPLETED], 0, 0, n_matches, string,
                      flags | MOUSEPAD_SEARCH_FLAGS_AREA_ALL_DOCUMENTS);
     }
 

commit c24fe9d7e87b1ee9b6fca12ee18e18aaa1b10253

search: Properly reset current match
Fixes: 9dae3d1457ab64a05ae586cac985479c7398e295
Related: !125

mousepad/mousepad-document.c
@@ -680,6 +680,9 @@ mousepad_document_search_completed_idle (gpointer data)
   else if (! (flags & MOUSEPAD_SEARCH_FLAGS_ACTION_NONE)
            && ! (flags & MOUSEPAD_SEARCH_FLAGS_AREA_SELECTION))
     gtk_text_buffer_place_cursor (document->buffer, &iter);
+  /* if e.g. a silent search fails by modifying the search entry this must be reset */
+  else
+    document->priv->cur_match = 0;
 
   /* force the signal emission, to cover cases where Mousepad search settings change without
    * changing GtkSourceView settings (e.g. when switching between single-document mode and