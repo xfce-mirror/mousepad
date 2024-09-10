xfce-mirror/mousepad

commit 32fe69a9d268a8fe874627f733b8b4bca44bfec8

Add support for admin protocol
Completes: ff06d7df6d563c588e666f6fee75b71711530df9

mousepad/mousepad-window.c
@@ -2039,6 +2039,38 @@ mousepad_window_file_is_open (MousepadWindow *window,
 
 
 
+static void
+admin_mount_finish (GObject *file,
+                    GAsyncResult *res,
+                    gpointer window)
+{
+  GError *error = NULL;
+  if (!g_file_mount_enclosing_volume_finish (G_FILE (file), res, &error))
+    {
+      if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_ALREADY_MOUNTED))
+        {
+          mousepad_dialogs_show_error (window, error, MOUSEPAD_MESSAGE_IO_ERROR_OPEN);
+          g_error_free (error);
+          return;
+        }
+      g_error_free (error);
+    }
+
+  /* block while user enters password */
+  g_file_query_exists (G_FILE (file), NULL);
+
+  /* normally done in mousepad_application_open() when opening file synchronously */
+  gtk_window_present (GTK_WINDOW (window));
+
+  mousepad_window_open_file (window, G_FILE (file),
+                             GPOINTER_TO_INT (mousepad_object_get_data (file, "admin-mount-encoding")),
+                             GPOINTER_TO_INT (mousepad_object_get_data (file, "admin-mount-line")),
+                             GPOINTER_TO_INT (mousepad_object_get_data (file, "admin-mount-column")),
+                             FALSE);
+}
+
+
+
 static gboolean
 mousepad_window_open_file (MousepadWindow *window,
                            GFile *file,
@@ -2060,6 +2092,17 @@ mousepad_window_open_file (MousepadWindow *window,
   /* check if the file is already open, if so focus its tab and exit */
   if (mousepad_window_file_is_open (window, file, TRUE))
     return TRUE;
+
+  /* deal with admin protocol asynchronously */
+  if (g_file_has_uri_scheme (file, "admin") && !mousepad_object_get_data (file, "admin-mount-done"))
+    {
+      mousepad_object_set_data (file, "admin-mount-done", GINT_TO_POINTER (TRUE));
+      mousepad_object_set_data (file, "admin-mount-encoding", GINT_TO_POINTER (encoding));
+      mousepad_object_set_data (file, "admin-mount-line", GINT_TO_POINTER (line));
+      mousepad_object_set_data (file, "admin-mount-column", GINT_TO_POINTER (column));
+      g_file_mount_enclosing_volume (file, G_MOUNT_MOUNT_NONE, NULL, NULL, admin_mount_finish, window);
+      return FALSE;
+    }
 
   /* new document */
   document = mousepad_document_new ();
@@ -4571,7 +4614,7 @@ mousepad_window_action_save (GSimpleAction *action,
       /* file is readonly */
       else if (G_UNLIKELY (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_PERMISSION_DENIED)
                            || g_error_matches (error, G_IO_ERROR, G_IO_ERROR_READ_ONLY)
-                           || mousepad_file_get_path (document->file) == NULL))
+                           || (!succeed && mousepad_file_get_path (document->file) == NULL)))
         {
           /* cleanup */
           g_clear_error (&error);

commit ec0814995f38f3e3ab8705b6f07a2e216a2ab05b

Take into account user cancellation when using the admin protocol
I suppose it should be done differently, since blocking via
g_file_query_info() (or any other GIO function) while the auth dialog is
shown sounds like a hack. But a quick look at the thunar or gedit code
shows things that are too complicated for me to deal with here, and
since the GIO doc isn't at all clear on how to handle it properly...

Fixes: 32fe69a9d268a8fe874627f733b8b4bca44bfec8

mousepad/mousepad-window.c
@@ -2044,7 +2044,9 @@ admin_mount_finish (GObject *file,
                     GAsyncResult *res,
                     gpointer window)
 {
+  GFileInfo *info;
   GError *error = NULL;
+
   if (!g_file_mount_enclosing_volume_finish (G_FILE (file), res, &error))
     {
       if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_ALREADY_MOUNTED))
@@ -2053,20 +2055,36 @@ admin_mount_finish (GObject *file,
           g_error_free (error);
           return;
         }
-      g_error_free (error);
+      g_clear_error (&error);
     }
 
   /* block while user enters password */
-  g_file_query_exists (G_FILE (file), NULL);
+  info = g_file_query_info (G_FILE (file), G_FILE_ATTRIBUTE_STANDARD_NAME, G_FILE_QUERY_INFO_NONE, NULL, &error);
+  if (info == NULL)
+    {
+      if (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_PERMISSION_DENIED))
+        {
+          /* G_IO_ERROR_PERMISSION_DENIED == operation cancelled by user: we just leave
+           * this callback and auth dialog will not be shown again */
+          g_error_free (error);
+          return;
+        }
+      g_error_free (error);
+    }
+  else
+    {
+      g_object_unref (info);
+    }
 
-  /* normally done in mousepad_application_open() when opening file synchronously */
-  gtk_window_present (GTK_WINDOW (window));
-
-  mousepad_window_open_file (window, G_FILE (file),
-                             GPOINTER_TO_INT (mousepad_object_get_data (file, "admin-mount-encoding")),
-                             GPOINTER_TO_INT (mousepad_object_get_data (file, "admin-mount-line")),
-                             GPOINTER_TO_INT (mousepad_object_get_data (file, "admin-mount-column")),
-                             FALSE);
+  /* at this point user should have authenticated successfully */
+  if (mousepad_window_open_file (window, G_FILE (file),
+                                 GPOINTER_TO_INT (mousepad_object_get_data (file, "admin-mount-encoding")),
+                                 GPOINTER_TO_INT (mousepad_object_get_data (file, "admin-mount-line")),
+                                 GPOINTER_TO_INT (mousepad_object_get_data (file, "admin-mount-column")),
+                                 FALSE))
+    {
+      gtk_window_present (GTK_WINDOW (window));
+    }
 }
 
 