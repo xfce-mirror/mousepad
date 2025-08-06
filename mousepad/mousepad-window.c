xfce-mirror/mousepad

commit 69a1cf07ca6fbf3196a93bb295357075288f1ae8

history: Extend recent history to autosaved notes
i.e. to pure autosaved files, or autosaved files without location set by
the user. This allows to restore cursor location, language, etc. in this
case too, which is actually not that different from the "normal" case.

mousepad/mousepad-window.c
@@ -2199,9 +2199,8 @@ retry:
             g_idle_add (mousepad_view_scroll_to_cursor,
                         mousepad_util_source_autoremove (window->active->textview));
 
-          /* insert in the recent history, don't pollute with autosave data */
-          if (autosave_uri == NULL)
-            mousepad_history_recent_add (document->file);
+          /* insert in the recent history */
+          mousepad_history_recent_add (document->file);
         }
       break;
 
@@ -2435,10 +2434,8 @@ mousepad_window_close_document (MousepadWindow *window,
   /* remove the document */
   if (succeed)
     {
-      /* store some data in the recent history if the file exists on disk */
-      if (mousepad_file_location_is_set (document->file)
-          && mousepad_util_query_exists (mousepad_file_get_location (document->file), TRUE))
-        mousepad_history_recent_add (document->file);
+      /* store some data in the recent history */
+      mousepad_history_recent_add (document->file);
 
       gtk_notebook_remove_page (notebook, gtk_notebook_page_num (notebook, GTK_WIDGET (document)));
     }
@@ -4023,8 +4020,9 @@ mousepad_window_recent_menu (GSimpleAction *action,
       /* walk through the items in the manager and pick the ones that are in the mousepad group */
       for (li = items; li != NULL; li = li->next)
         {
-          /* check if the item is in the Mousepad group */
-          if (!gtk_recent_info_has_group (li->data, MOUSEPAD_NAME))
+          /* check if the item is in the Mousepad group and isn't an autosaved note */
+          if (!gtk_recent_info_has_group (li->data, MOUSEPAD_NAME)
+              || gtk_recent_info_get_private_hint (li->data))
             continue;
 
           /* insert the list, sorted by date */

mousepad/mousepad-file.c
@@ -609,7 +609,7 @@ mousepad_file_location_is_set (MousepadFile *file)
 {
   g_return_val_if_fail (MOUSEPAD_IS_FILE (file), FALSE);
 
-  return file->location != NULL;
+  return file->location != NULL && !file->temporary;
 }
 
 

mousepad/mousepad-history.c
@@ -165,6 +165,13 @@ mousepad_history_recent_add (MousepadFile *file)
   if (MOUSEPAD_SETTING_GET_UINT (RECENT_MENU_ITEMS) == 0)
     return;
 
+  if (mousepad_file_location_is_set (file))
+    uri = mousepad_file_get_uri (file);
+  else if (mousepad_file_autosave_location_is_set (file))
+    uri = mousepad_file_autosave_get_uri (file);
+  else
+    return;
+
   /* get the data */
   charset = mousepad_encoding_get_charset (mousepad_file_get_encoding (file));
   buffer = mousepad_file_get_buffer (file);
@@ -188,10 +195,9 @@ mousepad_history_recent_add (MousepadFile *file)
   info.app_name = MOUSEPAD_NAME;
   info.app_exec = PACKAGE " %u";
   info.groups = groups;
-  info.is_private = FALSE;
+  info.is_private = !mousepad_file_location_is_set (file);
 
   /* add the new recent info to the recent manager */
-  uri = mousepad_file_get_uri (file);
   gtk_recent_manager_add_full (gtk_recent_manager_get_default (), uri, &info);
 
   /* cleanup */

commit 3e854550e459c3d2c55b34ab23117a69491ffe90

Consider temporary location as valid when saving
Save-as was broken due to previous commit.

Fixes: 69a1cf07ca6fbf3196a93bb295357075288f1ae8

mousepad/mousepad-window.c
@@ -4590,7 +4590,8 @@ mousepad_window_action_save (GSimpleAction *action,
   g_return_if_fail (MOUSEPAD_IS_WINDOW (window));
   g_return_if_fail (MOUSEPAD_IS_DOCUMENT (document));
 
-  if (!mousepad_file_location_is_set (document->file))
+  /* can be a temporary location: don't use mousepad_file_location_is_set() here */
+  if (mousepad_file_get_location (document->file) == NULL)
     {
       /* file has no filename yet, open the save as dialog */
       save_as = g_action_map_lookup_action (G_ACTION_MAP (window), "file.save-as");